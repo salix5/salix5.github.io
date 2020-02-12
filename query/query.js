8aaf754385ddc04c073b9394ba0c059707d9488b
salix5 <salix5@livemail.tw>
2020-02-12 12:17:27 +0800

    query: add div_half

--- query/query.js
+++ query/query.js
@@ -450,7 +450,7 @@ function create_rows(result){
 		div_result.insertBefore(table1, null);
 		var div_half = document.createElement('div');
 		div_half.className = 'half-line';
-		div_half.innerHTML = ' ';
+		div_half.innerHTML = '&nbsp;';
 		div_result.insertBefore(div_half, null);
 }
 
