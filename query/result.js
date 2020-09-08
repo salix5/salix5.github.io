8ecfba8ed59273d86dbadbea2efbfd7bfe31d03e
salix5 <salix5@livemail.tw>
2020-09-08 09:00:33 +0800

    query: update

---
+++ query/result.js
@@ -0,0 +1,218 @@
+"use strict";
+// table width
+const MAX_WIDTH = 700;	
+
+var result;
+
+function print_ad(x){
+	if(x == -2)
+		return '?';
+	else
+		return x;
+}
+
+function print_link(id, ot, db_id){
+	switch(id){
+		case 68811206:
+			return 'https://yugipedia.com/wiki/68811206'
+		default:
+			var url = 'https://www.db.yugioh-card.com/yugiohdb/card_search.action?ope=2&cid=' + db_id;
+			if(ot == 2)
+				return url + '&request_locale=en';
+			else
+				return url + '&request_locale=ja';
+	}
+}
+
+function print_limit(limit){
+	if(limit == 0)
+		return '<img src="0.png" height="20" width="20">';
+	else if(limit == 1)
+		return '<img src="1.png" height="20" width="20">';
+	else if(limit == 2)
+		return '<img src="2.png" height="20" width="20">';
+	else
+		return '';
+}
+
+function compare_card(a, b){
+	return a.name.localeCompare(b.name);
+}
+
+function create_rows(card){
+	//var div_card = document.getElementById('div_result');
+	var table1 = document.getElementById('table_result');
+	if(window.innerWidth > MAX_WIDTH)
+		table1.style.width = MAX_WIDTH + 'px';
+	else
+		table1.style.width = '90%';
+	var row1 = table1.insertRow(-1);
+	var cell1 = row1.insertCell(-1);
+	var cell2 = row1.insertCell(-1);
+	var cell3 = row1.insertCell(-1);
+
+	cell1.className = 'card_id';
+	cell2.className = 'query';
+	cell3.className = 'query';
+	if(card.id <= 99999999)
+		cell1.innerHTML = '<a href="' + print_link(card.id, card.ot, card.db_id) + '" target="_blank" rel="noreferrer">' + card.id.toString().padStart(8, '0') + '</a>';
+	else
+		cell1.innerHTML = card.id.toString();
+	
+	cell2.innerHTML = card.name + print_limit(card.limit);
+	if(card.ot == 2)
+		cell2.innerHTML += '<img src="tcg.png" height="20" width="40">';
+	cell3.innerHTML = card.jp_name;
+	
+	var mtype = '';
+	var subtype = '';
+	var lvstr = '等級';
+	var marker = '';
+	var data = '';
+	var output = '';
+	
+	if(card.type & TYPE_MONSTER){
+		mtype = '怪獸';
+		if(card.type & TYPE_RITUAL)
+			subtype = '/儀式';
+		else if(card.type & TYPE_FUSION)
+			subtype = '/融合';
+		else if(card.type & TYPE_SYNCHRO)
+			subtype = '/同步';
+		else if(card.type & TYPE_XYZ){
+			subtype = '/超量';
+			lvstr = '階級';
+		}
+		else if(card.type & TYPE_LINK){
+			subtype = '/連結';
+			lvstr = 'LINK-';
+		}
+		// extype
+		if(card.type & TYPE_PENDULUM){
+		           subtype += '/靈擺';
+		}
+		if(card.type & TYPE_NORMAL)
+			subtype += '/通常';
+		
+		if(card.type & TYPE_SPIRIT)
+			subtype += '/靈魂';
+		if(card.type & TYPE_UNION)
+			subtype += '/聯合';
+		if(card.type & TYPE_DUAL)
+			subtype += '/二重';
+		if(card.type & TYPE_TUNER)
+			subtype += '/協調';
+		if(card.type & TYPE_FLIP)
+			subtype += '/反轉';
+		if(card.type & TYPE_TOON)
+			subtype += '/卡通';
+		if(card.type & TYPE_SPSUMMON)
+			subtype += '/特殊召喚';
+		if(card.type & TYPE_EFFECT)
+			subtype += '/效果';
+		data = '[' + mtype + subtype + '] ';
+	}
+	else if(card.type & TYPE_SPELL){
+		mtype = '魔法';
+		if(card.type & TYPE_QUICKPLAY)
+			subtype = '速攻';
+		else if(card.type & TYPE_CONTINUOUS)
+			subtype = '永續';
+		else if(card.type & TYPE_EQUIP)
+			subtype = '裝備';
+		else if(card.type & TYPE_RITUAL)
+			subtype = '儀式';
+		else if(card.type & TYPE_FIELD)
+			subtype = '場地';
+		else
+			subtype = '通常';
+		data = '[' + subtype + mtype + '] ';
+	}
+	else if(card.type & TYPE_TRAP){
+		mtype = '陷阱';
+		if(card.type & TYPE_CONTINUOUS)
+			subtype = '永續';
+		else if(card.type & TYPE_COUNTER)
+			subtype = '反擊';
+		else
+			subtype = '通常';
+		data = '[' + subtype + mtype + '] ';
+	}
+	
+	if(card.type & TYPE_MONSTER){
+		data += lvstr + (card.level & 0xff);
+		data += '/' + attr_to_str[card.attribute];
+		data += '/' + race_to_str[card.race] + '族<br>'; 
+		data += print_ad(card.atk);
+		if(card.type & TYPE_LINK){
+			data += '/-';
+			marker = '<div class="marker">';
+			if(card.def & LINK_MARKER_TOP_LEFT)
+				marker += '<span class="ul t">▲</span>';
+			else
+				marker += '<span class="ul f">△</span>';
+			if(card.def & LINK_MARKER_TOP )
+				marker += '<span class="t">▲</span>';
+			else
+				marker += '<span class="f">△</span>';
+			if(card.def & LINK_MARKER_TOP_RIGHT)
+				marker += '<span class="ur t">▲</span>';
+			else
+				marker += '<span class="ur f">△</span>';
+	
+			marker += '<br>';
+			if(card.def & LINK_MARKER_LEFT)
+				marker += '<span class="l t">▲</span>';
+			else
+				marker += '<span class="l f">△</span>';
+			marker += '<span>　</span>';
+			if(card.def & LINK_MARKER_RIGHT)
+				marker += '<span class="r t">▲</span>';
+			else
+				marker += '<span class="r f">△</span>';
+			marker = marker + '<br>';
+	
+			if(card.def & LINK_MARKER_BOTTOM_LEFT)
+				marker += '<span class="dl t">▲</span>';
+			else
+				marker += '<span class="dl f">△</span>';
+			if(card.def & LINK_MARKER_BOTTOM )
+				marker += '<span class="d t">▲</span>';
+			else
+				marker += '<span class="d f">△</span>';
+			if(card.def & LINK_MARKER_BOTTOM_RIGHT)
+				marker += '<span class="dr t">▲</span>';
+			else
+				marker += '<span class="dr f">△</span>';
+			marker += '</div>';
+		}
+		else{
+			data +=  '/' + print_ad(card.def);
+		}
+		if(card.type & TYPE_PENDULUM){
+			data += '/刻度' + ((card.level >> 24) & 0xff);
+		}
+	}
+	output = '<span style="color: Blue;">' + data + '<br></span>' + marker;
+	output += card.desc.replace(/\n/g, "<br>");
+	var row_effect = table1.insertRow(-1);
+	var cell_effect = row_effect.insertCell(-1);
+	cell_effect.className = "query";
+	cell_effect.innerHTML = output;
+	cell_effect.colSpan = "2";
+	cell_effect.colSpan = "3";
+	
+	/*div_result.insertBefore(table1, null);
+	var div_half = document.createElement('div');
+	div_half.className = 'half-line';
+	div_half.innerHTML = '&nbsp;';
+	div_result.insertBefore(div_half, null);*/
+}
+
+function show_result(){
+	if(localStorage.getItem('result')){
+		result = JSON.parse(localStorage.getItem('result'));
+		result.sort(compare_card);
+		result.forEach(create_rows);
+	}
+}
--- query/webpage.js
+++ query/webpage.js
@@ -19,9 +19,9 @@ function add_opt(sel, value, text){
 function create_subtype(){
 	var select_type = document.getElementById('select_type');
 	var select_ao1 = document.getElementById('select_ao1');
-	var m = document.getElementById('subtype_m');
-	var s = document.getElementById('subtype_s');
-	var t = document.getElementById('subtype_t');
+	var subtype_m = document.getElementById('subtype_m');
+	var subtype_s = document.getElementById('subtype_s');
+	var subtype_t = document.getElementById('subtype_t');
 	var row_lv = document.getElementById('row_lv');
 	var row_sc = document.getElementById('row_sc');
 	var row_marker = document.getElementById('row_marker');
@@ -32,9 +32,9 @@ function create_subtype(){
 	
 	switch(select_type.value){
 		case '':
-			m.style.display = 'none';
-			s.style.display = 'none';
-			t.style.display = 'none';
+			subtype_m.style.display = 'none';
+			subtype_s.style.display = 'none';
+			subtype_t.style.display = 'none';
 			select_ao1.style.display = 'none';
 			row_lv.style.display = '';
 			row_sc.style.display = '';
@@ -45,9 +45,9 @@ function create_subtype(){
 			row_def.style.display = '';
 			break;
 		case 'm':
-			m.style.display = '';
-			s.style.display = 'none';
-			t.style.display = 'none';
+			subtype_m.style.display = '';
+			subtype_s.style.display = 'none';
+			subtype_t.style.display = 'none';
 			select_ao1.style.display = '';
 			row_lv.style.display = '';
 			row_sc.style.display = '';
@@ -58,9 +58,9 @@ function create_subtype(){
 			row_def.style.display = '';
 			break;
 		case 's':
-			m.style.display = 'none';
-			s.style.display = '';
-			t.style.display = 'none';
+			subtype_m.style.display = 'none';
+			subtype_s.style.display = '';
+			subtype_t.style.display = 'none';
 			select_ao1.style.display = 'none';
 			row_lv.style.display = 'none';
 			row_sc.style.display = 'none';
@@ -71,9 +71,9 @@ function create_subtype(){
 			row_def.style.display = 'none';
 			break;
 		case 't':
-			m.style.display = 'none';
-			s.style.display = 'none';
-			t.style.display = '';
+			subtype_m.style.display = 'none';
+			subtype_s.style.display = 'none';
+			subtype_t.style.display = '';
 			select_ao1.style.display = 'none';
 			row_lv.style.display = 'none';
 			row_sc.style.display = 'none';
@@ -92,6 +92,86 @@ function clear_cb(type){
 	for(let i = 0; i < cb_list.length; ++i){
 		cb_list[i].checked = false;
 	}
-	var rst = document.getElementById(type + '_reset');
-	rst.checked = false;
+	if(type != 'marker'){
+		var rst = document.getElementById(type + '_reset');
+		rst.checked = false;
+	}
+}
+
+function clear_query(){
+	var text_id = document.getElementById('text_id');
+	var text_name = document.getElementById('text_name');
+	var text_effect = document.getElementById('text_effect');
+	
+	var text_lv1 = document.getElementById('text_lv1');
+	var text_lv2 = document.getElementById('text_lv2');
+	var text_sc1 = document.getElementById('text_sc1');
+	var text_sc2 = document.getElementById('text_sc2');
+	
+	var text_atk1 = document.getElementById('text_atk1');
+	var text_atk2 = document.getElementById('text_atk2');
+	var text_def1 = document.getElementById('text_def1');
+	var text_def2 = document.getElementById('text_def2');
+	
+	var select_ot  = document.getElementById('select_ot');
+	var select_type = document.getElementById('select_type');
+	var select_ao1 = document.getElementById('select_ao1');
+	var select_ao2 = document.getElementById('select_ao2');
+	
+	var dm = document.getElementById('subtype_m');
+	var ds = document.getElementById('subtype_s');
+	var dt = document.getElementById('subtype_t');
+	
+	var row_lv = document.getElementById('row_lv');
+	var row_sc = document.getElementById('row_sc');
+	var row_marker = document.getElementById('row_marker');
+	var row_attr = document.getElementById('row_attr');
+	var row_race = document.getElementById('row_race');
+	var row_atk = document.getElementById('row_atk');
+	var row_def = document.getElementById('row_def');
+	
+	text_id.value = '';
+	text_name.value = '';
+	text_lv1.value = '';
+	text_lv2.value = '';
+	text_sc1.value = '';
+	text_sc2.value = '';
+	
+	text_atk1.value = '';
+	text_atk2.value = '';
+	text_def1.value = '';
+	text_def2.value = '';
+	text_effect.value = '';
+	
+	select_ot.selectedIndex = 0;
+	select_type.selectedIndex = 0;
+	select_ao1.selectedIndex = 0;
+	select_ao1.style.display = 'none';
+	select_ao2.selectedIndex = 0;
+	
+	clear_cb('mtype');
+	clear_cb('stype');
+	clear_cb('ttype');
+	dm.style.display = 'none';
+	ds.style.display = 'none';
+	dt.style.display = 'none';
+	row_lv.style.display = '';
+	row_sc.style.display = '';
+	row_marker.style.display = '';
+	row_attr.style.display = '';
+	row_race.style.display = '';
+	row_atk.style.display = '';
+	row_def.style.display = '';
+	
+	clear_cb('attr');
+	clear_cb('race');
+	clear_cb('marker');
+}
+
+function init(){
+	var button1 = document.getElementById('button1');
+	var button2 = document.getElementById('button2');
+	clear_query();
+	button1.disabled = true;
+	button2.disabled = true;
 }
