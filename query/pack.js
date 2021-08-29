ff7b2b9ab165edffa46dfa13fc1cd1cfcea54788
salix5 <salix5@livemail.tw>
2021-08-29 18:42:25 +0800

    query: update pack.js

--- query/pack.js
+++ query/pack.js
@@ -1,8 +1,6 @@
 var pre_release={
 	VJMP: 100200001,
 	
-	DBGC: 100417001,
-	
 	BACH: 101107001,
 	
 	WPP2: 100281001,
@@ -13,8 +11,6 @@ var pre_release={
 var wiki_link = {
 	VJMP: "https://yugioh-wiki.net/index.php?%BD%F1%C0%D2%C9%D5%C2%B0%A5%AB%A1%BC%A5%C9#VJ2021",
 	
-	DBGC: "https://yugioh-wiki.net/index.php?%A5%C7%A5%C3%A5%AD%A5%D3%A5%EB%A5%C9%A5%D1%A5%C3%A5%AF%20%A5%B0%A5%E9%A5%F3%A5%C9%A1%A6%A5%AF%A5%EA%A5%A8%A5%A4%A5%BF%A1%BC%A5%BA",
-
 	BACH: "https://yugioh-wiki.net/index.php?BATTLE%20OF%20CHAOS",
 	
 	WPP2: "https://yugioh-wiki.net/index.php?WORLD%20PREMIERE%20PACK%202021",
