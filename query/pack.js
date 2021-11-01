02dea8875f58a9f60f2e486c6bfb10c1dc2e78c5
salix5 <salix5@livemail.tw>
2021-11-02 01:07:28 +0800

    query: update pack list

--- query/pack.js
+++ query/pack.js
@@ -1,21 +1,22 @@
 var pre_release={
 	VJMP: 100200001,
 	
-	BACH: 101107001,
-	
 	YO04: 100283001,
 	
 	KC01: 100284001,
 	
 	DP26: 100426001,
+	
+	SD43: 100343001,
+	
+	SSB1: 100285001,
 
 	// TCG
+	_BODE: 101106000,
 };
 
 var wiki_link = {
 	VJMP: "https://yugioh-wiki.net/index.php?%BD%F1%C0%D2%C9%D5%C2%B0%A5%AB%A1%BC%A5%C9#VJ2021",
-	
-	BACH: "https://yugioh-wiki.net/index.php?BATTLE%20OF%20CHAOS",
 	
 	YO04: "https://yugioh-wiki.net/index.php?%BD%F1%C0%D2%C9%D5%C2%B0%A5%AB%A1%BC%A5%C9#YO",
 	
@@ -23,5 +24,10 @@ var wiki_link = {
 	
 	DP26: "https://yugioh-wiki.net/index.php?%A5%C7%A5%E5%A5%A8%A5%EA%A5%B9%A5%C8%A5%D1%A5%C3%A5%AF%A1%DD%BF%BC%CA%A5%A4%CE%A5%C7%A5%E5%A5%A8%A5%EA%A5%B9%A5%C8%CA%D4%A1%DD",
 	
+	SD43: "https://yugioh-wiki.net/index.php?%A5%B9%A5%C8%A5%E9%A5%AF%A5%C1%A5%E3%A1%BC%A5%C7%A5%C3%A5%AD%A1%DDALBA%20STRIKE%A1%DD",
+	
+	SSB1: "https://yugioh-wiki.net/index.php?SECRET%20SHINY%20BOX",
+	
 	// TCG
+	_BODE: "https://yugipedia.com/wiki/Burst_of_Destiny",
 };
