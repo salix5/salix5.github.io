7ded932d6005e22074f86d5291409dba24e639d5
salix5 <salix5@livemail.tw>
2021-12-24 23:15:42 +0800

    query: update pack list

--- query/pack.js
+++ query/pack.js
@@ -5,8 +5,6 @@ var pre_release={
 	
 	SSB1: 100285001,
 	
-	"22PP": 100286001,
-	
 	DIFO: 101108001,
 	
 	HC01: 100287001,
@@ -20,8 +18,6 @@ var wiki_link = {
 	KC01: "https://yugioh-wiki.net/index.php?%CD%B7%B5%BA%B2%A6OCG%A5%C7%A5%E5%A5%A8%A5%EB%A5%E2%A5%F3%A5%B9%A5%BF%A1%BC%A5%BA%2025th%20ANNIVERSARY%20ULTIMATE%20KAIBA%20SET",
 	
 	SSB1: "https://yugioh-wiki.net/index.php?SECRET%20SHINY%20BOX",
-	
-	"22PP": "https://yugioh-wiki.net/index.php?PREMIUM%20PACK#PP2022",
 	
 	DIFO: "https://yugioh-wiki.net/index.php?DIMENSION%20FORCE",
 	
