3459d5545cd91cb18d83bb506e31f101c6b16eff
salix5 <salix5@livemail.tw>
2021-12-27 22:57:10 +0800

    query: update path

--- query/result.js
+++ query/result.js
@@ -172,7 +172,7 @@ function create_rows(card){
 	let img_card = document.createElement('img');
 	img_card.className = 'pic';
 	if(card.id <= 99999999)
-		img_card.src = `https://salix5.github.io/CardEditor/pics/${card.id}.jpg`;
+		img_card.src = `https://salix5.github.io/query-data/pics/${card.id}.jpg`;
 	else
 		img_card.src = `../cdb/pics/${card.id}.jpg`;
 	img_card.onerror = imgError;
