851bd7644af9706c3bd38e8bec04702632371d5c
salix5 <salix5@livemail.tw>
2022-03-07 22:41:15 +0800

    query: update pack list

--- query/pack.js
+++ query/pack.js
@@ -1,9 +1,9 @@
+const zip_tag = "0307";
+
 var pre_release={
 	VJMP: 100200001,
 	
 	KC01: 100284001,
-	
-	HC01: 100287001,
 	
 	VX02: 100288001,
 	
@@ -12,15 +12,12 @@ var pre_release={
 	POTE: 101109001,
 
 	// TCG
-	_BACH: 101107000,
 };
 
 var wiki_link = {
 	VJMP: "https://yugioh-wiki.net/index.php?%BD%F1%C0%D2%C9%D5%C2%B0%A5%AB%A1%BC%A5%C9#VJ2021",
 	
 	KC01: "https://yugioh-wiki.net/index.php?%CD%B7%B5%BA%B2%A6OCG%A5%C7%A5%E5%A5%A8%A5%EB%A5%E2%A5%F3%A5%B9%A5%BF%A1%BC%A5%BA%2025th%20ANNIVERSARY%20ULTIMATE%20KAIBA%20SET",
-	
-	HC01: "https://yugioh-wiki.net/index.php?HISTORY%20ARCHIVE%20COLLECTION",
 	
 	VX02: "https://yugioh-wiki.net/index.php?%BD%F1%C0%D2%C9%D5%C2%B0%A5%AB%A1%BC%A5%C9#VX02",
 	
@@ -29,5 +26,4 @@ var wiki_link = {
 	POTE: "https://yugioh-wiki.net/index.php?POWER%20OF%20THE%20ELEMENTS",
 	
 	// TCG
-	_BACH: "https://yugipedia.com/wiki/Battle_of_Chaos",
 };
