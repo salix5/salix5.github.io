5471fa77d284cac9532a9e2fa832da5648c8ac75
salix5 <salix5@livemail.tw>
2022-05-21 23:34:25 +0800

    query: update pack

--- query/pack.js
+++ query/pack.js
@@ -1,8 +1,6 @@
 var pre_release={
 	VJMP: 100200001,
 	
-	DP27: 100427001,
-	
 	YO05: 100289001,
 	
 	AC02: 100290001,
@@ -19,8 +17,6 @@ var pre_release={
 
 var wiki_link = {
 	VJMP: "https://yugioh-wiki.net/index.php?%BD%F1%C0%D2%C9%D5%C2%B0%A5%AB%A1%BC%A5%C9#VJ2022",
-	
-	DP27: "https://yugioh-wiki.net/index.php?%A5%C7%A5%E5%A5%A8%A5%EA%A5%B9%A5%C8%A5%D1%A5%C3%A5%AF%A1%DD%B5%B1%C0%D0%A4%CE%A5%C7%A5%E5%A5%A8%A5%EA%A5%B9%A5%C8%CA%D4%A1%DD",
 	
 	YO05: "https://yugioh-wiki.net/index.php?%BD%F1%C0%D2%C9%D5%C2%B0%A5%AB%A1%BC%A5%C9#YO",
 	
