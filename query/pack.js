dccf7b1ef103925fb64304411aa6e8c0f7b365eb
salix5 <salix5@livemail.tw>
2022-06-14 15:55:16 +0800

    typos

--- query/pack.js
+++ query/pack.js
@@ -22,7 +22,7 @@ var wiki_link = {
 	
 	DABL: "https://yugioh-wiki.net/index.php?DARKWING%20BLAST",
 	
-	SR13: "https://yugioh-wiki.net/index.php?%A5%B9%A5%C8%A5%E9%A5%AF%A5%C1%A5%E3%A1%BC%A5%C7%A5%C3%A5%ADR%A1%DD%A5%C7%A5%D3%A5%EB%A5%BA%A1%A6%A5%B2%A1%BC%A5%C8%A1%DD#pack"
+	SR13: "https://yugioh-wiki.net/index.php?%A5%B9%A5%C8%A5%E9%A5%AF%A5%C1%A5%E3%A1%BC%A5%C7%A5%C3%A5%ADR%A1%DD%A5%C7%A5%D3%A5%EB%A5%BA%A1%A6%A5%B2%A1%BC%A5%C8%A1%DD",
 	
 	// TCG
 	_DIFO: "https://yugipedia.com/wiki/Dimension_Force",
