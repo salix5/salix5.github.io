26fa4ddd0389ef0db772737e6d2ab840f88dfdff
salix5 <salix5@livemail.tw>
2022-06-19 00:28:43 +0800

    update pack

--- query/pack.js
+++ query/pack.js
@@ -3,14 +3,11 @@ var pre_release={
 	
 	SD44: 100344001,
 	
-	VP22: 100291001,
-	
 	DABL: 101110001,
 	
 	SR13: 100313001,
 
 	// TCG
-	_DIFO: 101108000,
 };
 
 var wiki_link = {
@@ -18,12 +15,9 @@ var wiki_link = {
 	
 	SD44: "https://yugioh-wiki.net/index.php?%A5%B9%A5%C8%A5%E9%A5%AF%A5%C1%A5%E3%A1%BC%A5%C7%A5%C3%A5%AD%A1%DD%CA%F5%B6%CC%A4%CE%C5%C1%C0%E2%A1%DD",
 	
-	VP22: "https://yugioh-wiki.net/index.php?V%20JUMP%20EDITION#VP22A",
-	
 	DABL: "https://yugioh-wiki.net/index.php?DARKWING%20BLAST",
 	
 	SR13: "https://yugioh-wiki.net/index.php?%A5%B9%A5%C8%A5%E9%A5%AF%A5%C1%A5%E3%A1%BC%A5%C7%A5%C3%A5%ADR%A1%DD%A5%C7%A5%D3%A5%EB%A5%BA%A1%A6%A5%B2%A1%BC%A5%C8%A1%DD",
 	
 	// TCG
-	_DIFO: "https://yugipedia.com/wiki/Dimension_Force",
 };
