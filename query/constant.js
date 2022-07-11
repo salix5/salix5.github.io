81d808ed138d6a6184d510166d00d7853c7853b3
salix5 <salix5@livemail.tw>
2022-07-11 14:18:35 +0800

    query: update constant

--- query/constant.js
+++ query/constant.js
@@ -1,87 +1,87 @@
 // type
-const TYPE_MONSTER		=0x1		//怪兽卡
-const TYPE_SPELL		=0x2		//魔法卡
-const TYPE_TRAP			=0x4		//陷阱卡
+const TYPE_MONSTER		=0x1
+const TYPE_SPELL		=0x2
+const TYPE_TRAP			=0x4
 
 // color type
-const TYPE_NORMAL			=0x10		//通常怪兽
-const TYPE_EFFECT			=0x20		//效果
-const TYPE_FUSION			=0x40		//融合
-const TYPE_RITUAL			=0x80		//仪式
-const TYPE_SYNCHRO		=0x2000		//同调
-const TYPE_XYZ			=0x800000	//超量
-const TYPE_PENDULUM		=0x1000000	//灵摆
-const TYPE_LINK			=0x4000000	//连接
-const TYPE_EXT = TYPE_FUSION | TYPE_SYNCHRO | TYPE_XYZ | TYPE_LINK;
+const TYPE_NORMAL		=0x10
+const TYPE_EFFECT		=0x20
+const TYPE_FUSION		=0x40
+const TYPE_RITUAL		=0x80
+const TYPE_SYNCHRO		=0x2000
+const TYPE_XYZ			=0x800000
+const TYPE_PENDULUM		=0x1000000
+const TYPE_LINK			=0x4000000
+const TYPE_EXT = TYPE_FUSION | TYPE_SYNCHRO | TYPE_XYZ | TYPE_LINK
 
 // extype
-const TYPE_SPIRIT		=0x200		//灵魂
-const TYPE_UNION		=0x400		//同盟
-const TYPE_DUAL			=0x800		//二重
-const TYPE_TUNER		=0x1000		//调整
-const TYPE_TOKEN		=0x4000		//衍生物
-const TYPE_FLIP			=0x200000	//翻转
-const TYPE_TOON			=0x400000	//卡通
-const TYPE_SPSUMMON		=0x2000000	//特殊召唤
+const TYPE_SPIRIT		=0x200
+const TYPE_UNION		=0x400
+const TYPE_DUAL			=0x800
+const TYPE_TUNER		=0x1000
+const TYPE_TOKEN		=0x4000
+const TYPE_FLIP			=0x200000
+const TYPE_TOON			=0x400000
+const TYPE_SPSUMMON		=0x2000000
 
 // spell type
-const TYPE_QUICKPLAY		=0x10000	//速攻
-const TYPE_CONTINUOUS		=0x20000	//永续
-const TYPE_EQUIP			=0x40000	//装备
+const TYPE_QUICKPLAY		=0x10000
+const TYPE_CONTINUOUS		=0x20000
+const TYPE_EQUIP			=0x40000
 //const TYPE_RITUAL			=0x80
-const TYPE_FIELD			=0x80000	//场地
+const TYPE_FIELD			=0x80000
 
 // trap type
 //const TYPE_CONTINUOUS		=0x20000
-const TYPE_COUNTER		=0x100000	//反击
+const TYPE_COUNTER		=0x100000
 
 // race
-const RACE_WARRIOR		=0x1		//战士
-const RACE_SPELLCASTER	=0x2		//魔法师
-const RACE_FAIRY		=0x4		//天使
-const RACE_FIEND		=0x8		//恶魔
-const RACE_ZOMBIE		=0x10		//不死
-const RACE_MACHINE		=0x20		//机械
-const RACE_AQUA			=0x40		//水
-const RACE_PYRO			=0x80		//炎
-const RACE_ROCK			=0x100		//岩石
-const RACE_WINDBEAST	=0x200		//鸟兽
-const RACE_PLANT		=0x400		//植物
-const RACE_INSECT		=0x800		//昆虫
-const RACE_THUNDER		=0x1000			//雷
-const RACE_DRAGON		=0x2000		//龙
-const RACE_BEAST		=0x4000		//兽
-const RACE_BEASTWARRIOR	=0x8000			//兽战士
-const RACE_DINOSAUR		=0x10000		//恐龙
-const RACE_FISH			=0x20000		//鱼
-const RACE_SEASERPENT	=0x40000	//海龙
-const RACE_REPTILE		=0x80000		//爬虫类
-const RACE_PSYCHO		=0x100000	//念动力
-const RACE_DIVINE		=0x200000	//幻神兽
-const RACE_CREATORGOD	=0x400000	//创造神
-const RACE_WYRM			=0x800000		//幻龙
-const RACE_CYBERSE		=0x1000000		//电子界
+const RACE_WARRIOR		=0x1
+const RACE_SPELLCASTER	=0x2
+const RACE_FAIRY		=0x4
+const RACE_FIEND		=0x8
+const RACE_ZOMBIE		=0x10
+const RACE_MACHINE		=0x20
+const RACE_AQUA			=0x40
+const RACE_PYRO			=0x80
+const RACE_ROCK			=0x100
+const RACE_WINDBEAST	=0x200
+const RACE_PLANT		=0x400
+const RACE_INSECT		=0x800
+const RACE_THUNDER		=0x1000
+const RACE_DRAGON		=0x2000
+const RACE_BEAST		=0x4000
+const RACE_BEASTWARRIOR	=0x8000
+const RACE_DINOSAUR		=0x10000
+const RACE_FISH			=0x20000
+const RACE_SEASERPENT	=0x40000
+const RACE_REPTILE		=0x80000
+const RACE_PSYCHO		=0x100000
+const RACE_DIVINE		=0x200000
+const RACE_CREATORGOD	=0x400000
+const RACE_WYRM			=0x800000
+const RACE_CYBERSE		=0x1000000
 
 // attr
-const ATTRIBUTE_EARTH	=0x01		//地
-const ATTRIBUTE_WATER	=0x02		//水
-const ATTRIBUTE_FIRE	=0x04		//炎
-const ATTRIBUTE_WIND	=0x08		//风
-const ATTRIBUTE_LIGHT	=0x10		//光
-const ATTRIBUTE_DARK	=0x20		//暗
-const ATTRIBUTE_DIVINE	=0x40		//神
+const ATTRIBUTE_EARTH	=0x01
+const ATTRIBUTE_WATER	=0x02
+const ATTRIBUTE_FIRE	=0x04
+const ATTRIBUTE_WIND	=0x08
+const ATTRIBUTE_LIGHT	=0x10
+const ATTRIBUTE_DARK	=0x20
+const ATTRIBUTE_DIVINE	=0x40
 
 // Link Marker
-const LINK_MARKER_BOTTOM_LEFT	=0x001 // ↙
-const LINK_MARKER_BOTTOM		=0x002 // ↓
-const LINK_MARKER_BOTTOM_RIGHT	=0x004 // ↘
+const LINK_MARKER_BOTTOM_LEFT	=0x001
+const LINK_MARKER_BOTTOM		=0x002
+const LINK_MARKER_BOTTOM_RIGHT 	=0x004
 
-const LINK_MARKER_LEFT			=0x008 // ←
-const LINK_MARKER_RIGHT			=0x020 // →
+const LINK_MARKER_LEFT			=0x008
+const LINK_MARKER_RIGHT			=0x020
 
-const LINK_MARKER_TOP_LEFT		=0x040 // ↖
-const LINK_MARKER_TOP			=0x080 // ↑
-const LINK_MARKER_TOP_RIGHT		=0x100 // ↗
+const LINK_MARKER_TOP_LEFT		=0x040
+const LINK_MARKER_TOP			=0x080
+const LINK_MARKER_TOP_RIGHT		=0x100
 
 const attr_to_str = {
 	[ATTRIBUTE_EARTH]: '地',
