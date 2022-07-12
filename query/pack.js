0626c726a5e4961899b48b74bf89b82b331d4cb4
salix5 <salix5@livemail.tw>
2022-07-12 12:32:55 +0800

    update pack

--- query/pack.js
+++ query/pack.js
@@ -4,6 +4,8 @@ var pre_release={
 	DABL: 101110001,
 	
 	SR13: 100313001,
+	
+	DBAD: 100419001,
 
 	// TCG
 	_POTE: 101109000,
@@ -15,6 +17,8 @@ var wiki_link = {
 	DABL: "https://yugioh-wiki.net/index.php?DARKWING%20BLAST",
 	
 	SR13: "https://yugioh-wiki.net/index.php?%A5%B9%A5%C8%A5%E9%A5%AF%A5%C1%A5%E3%A1%BC%A5%C7%A5%C3%A5%ADR%A1%DD%A5%C7%A5%D3%A5%EB%A5%BA%A1%A6%A5%B2%A1%BC%A5%C8%A1%DD",
+	
+	DBAD: "https://yugioh-wiki.net/index.php?%A5%C7%A5%C3%A5%AD%A5%D3%A5%EB%A5%C9%A5%D1%A5%C3%A5%AF%20%A5%A2%A5%E1%A5%A4%A5%B8%A5%F3%A5%B0%A1%A6%A5%C7%A5%A3%A5%D5%A5%A7%A5%F3%A5%C0%A1%BC%A5%BA",
 	
 	// TCG
 	_POTE: "https://yugipedia.com/wiki/Power_of_the_Elements",
