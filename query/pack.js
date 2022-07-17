1211726abb96beb9f55b627ee1731f925d9c60aa
salix5 <salix5@livemail.tw>
2022-07-17 15:49:46 +0800

    update pack

--- query/pack.js
+++ query/pack.js
@@ -1,8 +1,6 @@
 var pre_release={
 	VJMP: 100200001,
 	
-	DABL: 101110001,
-	
 	SR13: 100313001,
 	
 	DBAD: 100419001,
@@ -15,8 +13,6 @@ var pre_release={
 
 var wiki_link = {
 	VJMP: "https://yugioh-wiki.net/index.php?%BD%F1%C0%D2%C9%D5%C2%B0%A5%AB%A1%BC%A5%C9#VJ2022",
-	
-	DABL: "https://yugioh-wiki.net/index.php?DARKWING%20BLAST",
 	
 	SR13: "https://yugioh-wiki.net/index.php?%A5%B9%A5%C8%A5%E9%A5%AF%A5%C1%A5%E3%A1%BC%A5%C7%A5%C3%A5%ADR%A1%DD%A5%C7%A5%D3%A5%EB%A5%BA%A1%A6%A5%B2%A1%BC%A5%C8%A1%DD",
 	
