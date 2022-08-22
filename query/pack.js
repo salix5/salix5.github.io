81cae48c9c03053e04e56d813e9b191f350f9da0
salix5 <salix5@livemail.tw>
2022-08-22 12:30:58 +0800

    update pack

--- query/pack.js
+++ query/pack.js
@@ -1,8 +1,6 @@
 var pre_release={
 	VJMP: 100200001,
 	
-	DBAD: 100419001,
-	
 	WPP3: 100292001,
 	
 	PHHY: 101111001,
@@ -12,8 +10,6 @@ var pre_release={
 
 var wiki_link = {
 	VJMP: "https://yugioh-wiki.net/index.php?%BD%F1%C0%D2%C9%D5%C2%B0%A5%AB%A1%BC%A5%C9#VJ2022",
-	
-	DBAD: "https://yugioh-wiki.net/index.php?%A5%C7%A5%C3%A5%AD%A5%D3%A5%EB%A5%C9%A5%D1%A5%C3%A5%AF%20%A5%A2%A5%E1%A5%A4%A5%B8%A5%F3%A5%B0%A1%A6%A5%C7%A5%A3%A5%D5%A5%A7%A5%F3%A5%C0%A1%BC%A5%BA",
 	
 	WPP3: "https://yugioh-wiki.net/index.php?WORLD%20PREMIERE%20PACK%202022",
 	
