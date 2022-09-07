63da258fed9cc4d28b8cf57e4f54d04da65c196c
salix5 <salix5@livemail.tw>
2022-09-07 14:38:24 +0800

    update pack

--- query/pack.js
+++ query/pack.js
@@ -1,16 +1,16 @@
 var pre_release={
 	VJMP: 100200001,
-	WPP3: 100292001,
+	//WPP3: 100292001,
 	PHHY: 101111001,
 	
 	// TCG
 	_YCSW: 100201001,
-	_MP22: 100293001,
+	_MP22: 100292001,
 };
 
 var wiki_link = {
 	VJMP: "https://yugioh-wiki.net/index.php?%BD%F1%C0%D2%C9%D5%C2%B0%A5%AB%A1%BC%A5%C9#VJ2022",
-	WPP3: "https://yugioh-wiki.net/index.php?WORLD%20PREMIERE%20PACK%202022",
+	//WPP3: "https://yugioh-wiki.net/index.php?WORLD%20PREMIERE%20PACK%202022",
 	PHHY: "https://yugioh-wiki.net/index.php?PHOTON%20HYPERNOVA",
 	
 	// TCG
