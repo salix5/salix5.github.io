a5285c84e084b2480c82457aa252b25966f9fe6e
salix5 <salix5@livemail.tw>
2022-09-14 22:24:53 +0800

    update pack

--- query/pack.js
+++ query/pack.js
@@ -1,6 +1,7 @@
 var pre_release={
 	VJMP: 100200001,
 	PHHY: 101111001,
+	SLT5: 100293001,
 	
 	// TCG
 };
@@ -8,6 +9,7 @@ var pre_release={
 var wiki_link = {
 	VJMP: "https://yugioh-wiki.net/index.php?%BD%F1%C0%D2%C9%D5%C2%B0%A5%AB%A1%BC%A5%C9#VJ2022",
 	PHHY: "https://yugioh-wiki.net/index.php?PHOTON%20HYPERNOVA",
+	SLT5: "https://yugioh-wiki.net/index.php?SELECTION%205",
 	
 	// TCG
 };
