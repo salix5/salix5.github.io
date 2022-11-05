2dca9de01fff4bf51127471307ae4e9379b20302
salix5 <salix5@livemail.tw>
2022-11-05 17:57:33 +0800

    update pack

--- query/pack.js
+++ query/pack.js
@@ -1,8 +1,7 @@
-var last_pack = "PHHY";
+var last_pack = "SLF1#1";
 
 var pre_release={
 	VJMP: 100200001,
-	SLF1: 100293001,
 	SD45: 100345001,
 	SUB1: 100294001,
 	
@@ -13,7 +12,6 @@ var pre_release={
 
 var wiki_link = {
 	VJMP: "https://yugioh-wiki.net/index.php?%BD%F1%C0%D2%C9%D5%C2%B0%A5%AB%A1%BC%A5%C9#VJ2022",
-	SLF1: "https://yugioh-wiki.net/index.php?SELECTION%205",
 	SD45: "https://yugioh-wiki.net/index.php?%A5%B9%A5%C8%A5%E9%A5%AF%A5%C1%A5%E3%A1%BC%A5%C7%A5%C3%A5%AD%A1%DD%EA%B5%CF%C7%CB%E2%A4%CE%BF%B9%A1%DD",
 	SUB1: "https://yugioh-wiki.net/index.php?SECRET%20UTILITY%20BOX",
 	
