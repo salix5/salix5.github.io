6d779ec219c0a4bf1b61f64b072fcbad7ec1cf35
salix5 <salix5@livemail.tw>
2022-11-08 18:05:53 +0800

    update pack

--- query/pack.js
+++ query/pack.js
@@ -11,7 +11,7 @@ var pre_release={
 };
 
 var wiki_link = {
-	VJMP: "https://yugioh-wiki.net/index.php?%BD%F1%C0%D2%C9%D5%C2%B0%A5%AB%A1%BC%A5%C9#VJ2022",
+	VJMP: "https://yugioh-wiki.net/index.php?%BD%F1%C0%D2%C9%D5%C2%B0%A5%AB%A1%BC%A5%C9#VJ2023",
 	SD45: "https://yugioh-wiki.net/index.php?%A5%B9%A5%C8%A5%E9%A5%AF%A5%C1%A5%E3%A1%BC%A5%C7%A5%C3%A5%AD%A1%DD%EA%B5%CF%C7%CB%E2%A4%CE%BF%B9%A1%DD",
 	SUB1: "https://yugioh-wiki.net/index.php?SECRET%20UTILITY%20BOX",
 	YO06: "https://yugioh-wiki.net/index.php?%BD%F1%C0%D2%C9%D5%C2%B0%A5%AB%A1%BC%A5%C9#YO",
