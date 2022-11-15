021d10fec2e3fd137bc8b0284170cb0b4b8828e9
salix5 <salix5@livemail.tw>
2022-11-15 22:00:57 +0800

    update pack

--- query/pack.js
+++ query/pack.js
@@ -6,6 +6,7 @@ var pre_release={
 	SD45: 100345001,
 	SUB1: 100294001,
 	YO06: 100295001,
+	CYAC: 101112001,
 	
 	// TCG
 };
@@ -15,6 +16,7 @@ var wiki_link = {
 	SD45: "https://yugioh-wiki.net/index.php?%A5%B9%A5%C8%A5%E9%A5%AF%A5%C1%A5%E3%A1%BC%A5%C7%A5%C3%A5%AD%A1%DD%EA%B5%CF%C7%CB%E2%A4%CE%BF%B9%A1%DD",
 	SUB1: "https://yugioh-wiki.net/index.php?SECRET%20UTILITY%20BOX",
 	YO06: "https://yugioh-wiki.net/index.php?%BD%F1%C0%D2%C9%D5%C2%B0%A5%AB%A1%BC%A5%C9#YO",
+	CYAC: "https://yugioh-wiki.net/index.php?CYBERSTORM%20ACCESS",
 	
 	// TCG
 };
