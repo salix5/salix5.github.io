d84b23812e7889c148b71843f8aa0916a35b7887
salix5 <salix5@livemail.tw>
2022-11-30 20:24:12 +0800

    query: case insensitive

--- query/result.js
+++ query/result.js
@@ -200,8 +200,8 @@ function create_rows(card, pack) {
 	}
 
 	// limit
-	if (ltable[card.id] !== undefined || ltable_md[card.id] !== undefined) {
-		card_alias += `OCG：${print_limit(ltable[card.id])} / MD：${print_limit(ltable_md[card.id])}<br>`;
+	if (card.id <= 99999999 && ltable[card.id] !== undefined || ltable_md[card.id] !== undefined) {
+		card_alias += `<br>OCG：${print_limit(ltable[card.id])} / MD：${print_limit(ltable_md[card.id])}<br>`;
 	}
 
 	let row_pic = table_result.insertRow(-1);
