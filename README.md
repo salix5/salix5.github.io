cb833302c24d401173d4c4741ebe5f308f666653
salix5 <salix5@livemail.tw>
2023-01-15 21:14:32 +0800

    update pack

--- README.md
+++ README.md
@@ -57,12 +57,14 @@ Ex.
 [字段對照表](https://docs.google.com/spreadsheets/d/1W-PvJDVEdpd-A8EYSRjPcWoKQwqSqytm9Arx0xZvMjs/edit?usp=sharing)
 
 ## 更新紀錄
+23/1/15  
+新增卡包：CYAC (1112)
+
 23/1/9  
 YO06：紅戀的麗傑-布拉達曼特
 
 23/1/3  
 復刻更新：23TP Vol.1  
-先行卡更新：CYAC (1112)
 
 19/4/1  
 卡名統一採用「砲」（石部砲）、「鎖鏈」
--- query/pack.js
+++ query/pack.js
@@ -1,9 +1,8 @@
 "use strict";
-const last_pack = "SUB1#3";
+const last_pack = "CYAC#1";
 
 var pre_release={
 	VJMP: 100200001,
-	CYAC: 101112001,
 	
 	// TCG
 	_PHHY: 101111000,
@@ -11,7 +10,6 @@ var pre_release={
 
 var wiki_link = {
 	VJMP: "https://yugioh-wiki.net/index.php?%BD%F1%C0%D2%C9%D5%C2%B0%A5%AB%A1%BC%A5%C9#VJ2023",
-	CYAC: "https://yugioh-wiki.net/index.php?CYBERSTORM%20ACCESS",
 	
 	// TCG
 	_PHHY: "https://yugipedia.com/wiki/Photon_Hypernova",
