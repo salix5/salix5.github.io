833d87b97130a213e7a451884c9ebd193b94a569
salix5 <salix5@livemail.tw>
2023-02-12 00:12:14 +0800

    update pack

--- query/pack.js
+++ query/pack.js
@@ -5,6 +5,7 @@ var pre_release={
 	VJMP: 100200001,
 	DBWS: 100420001,
 	DUNE: 101201001,
+	VX03: 100205001,
 	
 	// TCG
 	_PHHY: 101111000,
@@ -14,6 +15,7 @@ var wiki_link = {
 	VJMP: "https://yugioh-wiki.net/index.php?%BD%F1%C0%D2%C9%D5%C2%B0%A5%AB%A1%BC%A5%C9#VJ2023",
 	DBWS: "https://yugioh-wiki.net/index.php?%A5%C7%A5%C3%A5%AD%A5%D3%A5%EB%A5%C9%A5%D1%A5%C3%A5%AF%20%A5%EF%A5%A4%A5%EB%A5%C9%A1%A6%A5%B5%A5%D0%A5%A4%A5%D0%A1%BC%A5%BA",
 	DUNE: "https://yugioh-wiki.net/index.php?DUELIST%20NEXUS",
+	VX03: "https://yugioh-wiki.net/index.php?%BD%F1%C0%D2%C9%D5%C2%B0%A5%AB%A1%BC%A5%C9#VB",
 	
 	// TCG
 	_PHHY: "https://yugipedia.com/wiki/Photon_Hypernova",
