d6f013eee1dca6c79fb0a02d45360117c24ae31e
salix5 <salix5566@gmail.com>
2023-03-07 23:44:55 +0800

    query: add ygo-query

--- query/query.js
+++ query/query.js
@@ -5,7 +5,7 @@ const extra_url = "../cdb/pre-release.cdb";
 const MAX_DIGIT = 10;
 const MAX_STRLEN = 200;
 
-var config = {
+const config = {
 	locateFile: filename => `./dist/${filename}`
 }
 var SQL;
@@ -35,119 +35,8 @@ String.prototype.toFullWidth = function () {
 	return this.replace(/[A-Za-z0-9]/g, function (s) { return String.fromCharCode(s.charCodeAt(0) + 0xFEE0); });
 };
 
-function is_alternative(id, alias, type) {
-	if (type & TYPE_TOKEN)
-		return alias !== 0;
-	else
-		return Math.abs(id - alias) < 10;
-}
-
-function query_card(db, qstr, arg, ret) {
-	let stmt = db.prepare(qstr);
-	stmt.bind(arg);
-
-	let inv_pack = Object.create(null);
-	if (arg.pack && pack_list[arg.pack]) {
-		for (let i = 0; i < pack_list[arg.pack].length; ++i) {
-			if (pack_list[arg.pack][i] !== 0 && pack_list[arg.pack][i] !== 1)
-				inv_pack[pack_list[arg.pack][i]] = i;
-		}
-	}
-
-	while (stmt.step()) {
-		let card = stmt.getAsObject();
-
-		// spell & trap reset data
-		if (card.type & (TYPE_SPELL | TYPE_TRAP)) {
-			card.atk = 0;
-			card.def = 0;
-			card.level = 0;
-			card.race = 0;
-			card.attribute = 0;
-		}
-		card.scale = (card.level >> 24) & 0xff;
-		card.level = card.level & 0xff;
-
-		// color
-		if (card.type & TYPE_MONSTER) {
-			if (!(card.type & TYPE_EXT)) {
-				if (card.type & TYPE_TOKEN)
-					card.color = 0;
-				else if (card.type & TYPE_NORMAL)
-					card.color = 1;
-				else if (card.type & TYPE_RITUAL)
-					card.color = 3;
-				else if (card.type & TYPE_EFFECT)
-					card.color = 2;
-				else
-					card.color = null;
-			}
-			else {
-				if (card.type & TYPE_FUSION)
-					card.color = 4;
-				else if (card.type & TYPE_SYNCHRO)
-					card.color = 5;
-				else if (card.type & TYPE_XYZ)
-					card.color = 6;
-				else if (card.type & TYPE_LINK)
-					card.color = 7;
-				else
-					card.color = null;
-			}
-		}
-		else if (card.type & TYPE_SPELL) {
-			if (card.type === TYPE_SPELL)
-				card.color = 10;
-			else if (card.type & TYPE_QUICKPLAY)
-				card.color = 11;
-			else if (card.type & TYPE_CONTINUOUS)
-				card.color = 12;
-			else if (card.type & TYPE_EQUIP)
-				card.color = 13;
-			else if (card.type & TYPE_RITUAL)
-				card.color = 14;
-			else if (card.type & TYPE_FIELD)
-				card.color = 15;
-			else
-				card.color = null;
-		}
-		else if (card.type & TYPE_TRAP) {
-			if (card.type === TYPE_TRAP)
-				card.color = 20;
-			else if (card.type & TYPE_CONTINUOUS)
-				card.color = 21;
-			else if (card.type & TYPE_COUNTER)
-				card.color = 22;
-			else
-				card.color = null;
-		}
-		else {
-			card.color = null;
-		}
-
-		// cid
-		card.cid = cid_table[card.id] ? cid_table[card.id] : null;
-		card.jp_name = name_table[card.id] ? name_table[card.id] : null;
-		card.en_name = name_table_en[card.id] ? name_table_en[card.id] : null;
-
-		// pack_id
-		if (card.id <= 99999999) {
-			if (arg.pack && pack_list[arg.pack])
-				card.pack_id = inv_pack[card.id];
-			else
-				card.pack_id = null;
-		}
-		else {
-			card.pack_id = card.id % 1000;
-		}
-		ret.push(card);
-	}
-	stmt.free();
-}
-
-const promise_db = fetch(`https://salix5.github.io/CardEditor/cards.zip`).then(response => response.blob()).then(JSZip.loadAsync).then(zip_file => zip_file.files["cards.cdb"].async("uint8array"));
+const promise_db = fetch("https://salix5.github.io/CardEditor/cards.zip").then(response => response.blob()).then(JSZip.loadAsync).then(zip_file => zip_file.files["cards.cdb"].async("uint8array"));
 const promise_db2 = fetch(extra_url).then(response => response.arrayBuffer()).then(buf => new Uint8Array(buf));
-const promise_sql = initSqlJs(config).then(response => { SQL = response; });
 const promise_lflist2 = fetch("text/lflist_md.json").then(response => response.json()).then(data => Object.assign(ltable_md, data));
 
 var promise_text = null;
@@ -182,7 +71,8 @@ else {
 	});
 }
 
-Promise.all([promise_sql, promise_db, promise_db2, promise_lflist2, promise_text]).then(function (values) {
+Promise.all([initSqlJs(config), promise_db, promise_db2, promise_lflist2, promise_text]).then(function (values) {
+	SQL = values[0];
 	db = new SQL.Database(values[1]);
 	db2 = new SQL.Database(values[2]);
 	url_query();
@@ -254,17 +144,6 @@ function check_str(val) {
 	if (!is_str(val))
 		return '';
 	return val;
-}
-
-function pack_cmd(pack) {
-	let cmd = '';
-	cmd = ` AND (0`;
-	for (let i = 0; i < pack.length; ++i) {
-		if (pack[i] !== 0 && pack[i] !== 1)
-			cmd += ` OR datas.id=${pack[i]}`;
-	}
-	cmd += `)`;
-	return cmd;
 }
 
 var id_to_type = {
@@ -1094,10 +973,4 @@ function param_to_condition(params, arg) {
 		}
 	}
 	return qstr;
-}
-
-function query(qstr, arg, ret) {
-	ret.length = 0;
-	query_card(db, qstr, arg, ret);
-	query_card(db2, qstr, arg, ret);
 }
---
+++ query/ygo-query.js
@@ -0,0 +1,145 @@
+﻿"use strict";
+
+const default_query1 = `SELECT datas.id, ot, alias, type, atk, def, level, attribute, race, name, desc FROM datas, texts WHERE datas.id == texts.id AND abs(datas.id - alias) >= 10 AND NOT type & ${TYPE_TOKEN}`;
+const default_query2 = `SELECT datas.id FROM datas, texts WHERE datas.id == texts.id AND alias == 0 AND NOT type & ${TYPE_TOKEN}`;
+
+// print condition for cards in pack
+function pack_cmd(pack) {
+	let cmd = '';
+	cmd = ` AND (0`;
+	for (let i = 0; i < pack.length; ++i) {
+		if (pack[i] !== 0 && pack[i] !== 1)
+			cmd += ` OR datas.id=${pack[i]}`;
+	}
+	cmd += `)`;
+	return cmd;
+}
+
+// check if card is alternative art
+function is_alternative(card) {
+	if (card.type & TYPE_TOKEN)
+		return card.alias !== 0;
+	else
+		return Math.abs(card.id - card.alias) < 10;
+}
+
+/*
+ * query cards in db and push into ret
+ * qstr: sqlite command
+ * arg: bindind object
+*/
+function query_card(db, qstr, arg, ret) {
+    let stmt = db.prepare(qstr);
+    stmt.bind(arg);
+
+	// pack_id
+    let inv_pack = Object.create(null);
+    if (arg.pack && pack_list[arg.pack]) {
+        for (let i = 0; i < pack_list[arg.pack].length; ++i) {
+            if (pack_list[arg.pack][i] !== 0 && pack_list[arg.pack][i] !== 1)
+                inv_pack[pack_list[arg.pack][i]] = i;
+        }
+    }
+
+    while (stmt.step()) {
+        let card = stmt.getAsObject();
+
+        // spell & trap reset data
+        if (card.type & (TYPE_SPELL | TYPE_TRAP)) {
+            card.atk = 0;
+            card.def = 0;
+            card.level = 0;
+            card.race = 0;
+            card.attribute = 0;
+        }
+        card.scale = (card.level >> 24) & 0xff;
+        card.level = card.level & 0xff;
+
+        // color
+        if (card.type & TYPE_MONSTER) {
+            if (!(card.type & TYPE_EXT)) {
+                if (card.type & TYPE_TOKEN)
+                    card.color = 0;
+                else if (card.type & TYPE_NORMAL)
+                    card.color = 1;
+                else if (card.type & TYPE_RITUAL)
+                    card.color = 3;
+                else if (card.type & TYPE_EFFECT)
+                    card.color = 2;
+
+                else
+                    card.color = null;
+            }
+            else {
+                if (card.type & TYPE_FUSION)
+                    card.color = 4;
+                else if (card.type & TYPE_SYNCHRO)
+                    card.color = 5;
+                else if (card.type & TYPE_XYZ)
+                    card.color = 6;
+                else if (card.type & TYPE_LINK)
+                    card.color = 7;
+
+                else
+                    card.color = null;
+            }
+        }
+        else if (card.type & TYPE_SPELL) {
+            if (card.type === TYPE_SPELL)
+                card.color = 10;
+            else if (card.type & TYPE_QUICKPLAY)
+                card.color = 11;
+            else if (card.type & TYPE_CONTINUOUS)
+                card.color = 12;
+            else if (card.type & TYPE_EQUIP)
+                card.color = 13;
+            else if (card.type & TYPE_RITUAL)
+                card.color = 14;
+            else if (card.type & TYPE_FIELD)
+                card.color = 15;
+
+            else
+                card.color = null;
+        }
+        else if (card.type & TYPE_TRAP) {
+            if (card.type === TYPE_TRAP)
+                card.color = 20;
+            else if (card.type & TYPE_CONTINUOUS)
+                card.color = 21;
+            else if (card.type & TYPE_COUNTER)
+                card.color = 22;
+
+            else
+                card.color = null;
+        }
+        else {
+            card.color = null;
+        }
+
+        // cid
+        card.cid = cid_table[card.id] ? cid_table[card.id] : null;
+        card.jp_name = name_table[card.id] ? name_table[card.id] : null;
+        card.en_name = name_table_en[card.id] ? name_table_en[card.id] : null;
+
+        // pack_id
+        if (card.id <= 99999999) {
+            if (arg.pack && pack_list[arg.pack])
+                card.pack_id = inv_pack[card.id];
+
+            else
+                card.pack_id = null;
+        }
+        else {
+            card.pack_id = card.id % 1000;
+        }
+        ret.push(card);
+    }
+    stmt.free();
+}
+
+// query cards in main db and pre-release db
+function query(qstr, arg, ret) {
+	ret.length = 0;
+	query_card(db, qstr, arg, ret);
+	query_card(db2, qstr, arg, ret);
+}
