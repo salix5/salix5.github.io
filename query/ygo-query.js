d96c417b97583c870636f2702d74d0cd2739b832
salix5 <salix5566@gmail.com>
2023-03-26 20:54:57 +0800

    query: format

--- query/ygo-query.js
+++ query/ygo-query.js
@@ -90,111 +90,111 @@ function is_alternative(card) {
  * arg: bindind object
 */
 function query_card(db, qstr, arg, ret) {
-    let stmt = db.prepare(qstr);
-    stmt.bind(arg);
+	let stmt = db.prepare(qstr);
+	stmt.bind(arg);
 
 	// pack_id
-    let inv_pack = Object.create(null);
-    if (arg.pack && pack_list[arg.pack]) {
-        for (let i = 0; i < pack_list[arg.pack].length; ++i) {
-            if (pack_list[arg.pack][i] !== 0 && pack_list[arg.pack][i] !== 1)
-                inv_pack[pack_list[arg.pack][i]] = i;
-        }
-    }
+	let inv_pack = Object.create(null);
+	if (arg.pack && pack_list[arg.pack]) {
+		for (let i = 0; i < pack_list[arg.pack].length; ++i) {
+			if (pack_list[arg.pack][i] !== 0 && pack_list[arg.pack][i] !== 1)
+				inv_pack[pack_list[arg.pack][i]] = i;
+		}
+	}
 
-    while (stmt.step()) {
-        let card = stmt.getAsObject();
+	while (stmt.step()) {
+		let card = stmt.getAsObject();
 
-        // spell & trap reset data
-        if (card.type & (TYPE_SPELL | TYPE_TRAP)) {
-            card.atk = 0;
-            card.def = 0;
-            card.level = 0;
-            card.race = 0;
-            card.attribute = 0;
-        }
-        card.scale = (card.level >> 24) & 0xff;
-        card.level = card.level & 0xff;
+		// spell & trap reset data
+		if (card.type & (TYPE_SPELL | TYPE_TRAP)) {
+			card.atk = 0;
+			card.def = 0;
+			card.level = 0;
+			card.race = 0;
+			card.attribute = 0;
+		}
+		card.scale = (card.level >> 24) & 0xff;
+		card.level = card.level & 0xff;
 
-        // color
-        if (card.type & TYPE_MONSTER) {
-            if (!(card.type & TYPE_EXT)) {
-                if (card.type & TYPE_TOKEN)
-                    card.color = 0;
-                else if (card.type & TYPE_NORMAL)
-                    card.color = 1;
-                else if (card.type & TYPE_RITUAL)
-                    card.color = 3;
-                else if (card.type & TYPE_EFFECT)
-                    card.color = 2;
+		// color
+		if (card.type & TYPE_MONSTER) {
+			if (!(card.type & TYPE_EXT)) {
+				if (card.type & TYPE_TOKEN)
+					card.color = 0;
+				else if (card.type & TYPE_NORMAL)
+					card.color = 1;
+				else if (card.type & TYPE_RITUAL)
+					card.color = 3;
+				else if (card.type & TYPE_EFFECT)
+					card.color = 2;
 
-                else
-                    card.color = null;
-            }
-            else {
-                if (card.type & TYPE_FUSION)
-                    card.color = 4;
-                else if (card.type & TYPE_SYNCHRO)
-                    card.color = 5;
-                else if (card.type & TYPE_XYZ)
-                    card.color = 6;
-                else if (card.type & TYPE_LINK)
-                    card.color = 7;
+				else
+					card.color = null;
+			}
+			else {
+				if (card.type & TYPE_FUSION)
+					card.color = 4;
+				else if (card.type & TYPE_SYNCHRO)
+					card.color = 5;
+				else if (card.type & TYPE_XYZ)
+					card.color = 6;
+				else if (card.type & TYPE_LINK)
+					card.color = 7;
 
-                else
-                    card.color = null;
-            }
-        }
-        else if (card.type & TYPE_SPELL) {
-            if (card.type === TYPE_SPELL)
-                card.color = 10;
-            else if (card.type & TYPE_QUICKPLAY)
-                card.color = 11;
-            else if (card.type & TYPE_CONTINUOUS)
-                card.color = 12;
-            else if (card.type & TYPE_EQUIP)
-                card.color = 13;
-            else if (card.type & TYPE_RITUAL)
-                card.color = 14;
-            else if (card.type & TYPE_FIELD)
-                card.color = 15;
+				else
+					card.color = null;
+			}
+		}
+		else if (card.type & TYPE_SPELL) {
+			if (card.type === TYPE_SPELL)
+				card.color = 10;
+			else if (card.type & TYPE_QUICKPLAY)
+				card.color = 11;
+			else if (card.type & TYPE_CONTINUOUS)
+				card.color = 12;
+			else if (card.type & TYPE_EQUIP)
+				card.color = 13;
+			else if (card.type & TYPE_RITUAL)
+				card.color = 14;
+			else if (card.type & TYPE_FIELD)
+				card.color = 15;
 
-            else
-                card.color = null;
-        }
-        else if (card.type & TYPE_TRAP) {
-            if (card.type === TYPE_TRAP)
-                card.color = 20;
-            else if (card.type & TYPE_CONTINUOUS)
-                card.color = 21;
-            else if (card.type & TYPE_COUNTER)
-                card.color = 22;
+			else
+				card.color = null;
+		}
+		else if (card.type & TYPE_TRAP) {
+			if (card.type === TYPE_TRAP)
+				card.color = 20;
+			else if (card.type & TYPE_CONTINUOUS)
+				card.color = 21;
+			else if (card.type & TYPE_COUNTER)
+				card.color = 22;
 
-            else
-                card.color = null;
-        }
-        else {
-            card.color = null;
-        }
+			else
+				card.color = null;
+		}
+		else {
+			card.color = null;
+		}
 
-        // cid
-        card.cid = cid_table[card.id] ? cid_table[card.id] : null;
-        card.jp_name = name_table[card.id] ? name_table[card.id] : null;
-        card.en_name = name_table_en[card.id] ? name_table_en[card.id] : null;
+		// cid
+		card.cid = cid_table[card.id] ? cid_table[card.id] : null;
+		card.jp_name = name_table[card.id] ? name_table[card.id] : null;
+		card.en_name = name_table_en[card.id] ? name_table_en[card.id] : null;
 
-        // pack_id
-        if (card.id <= 99999999) {
-            if (arg.pack && pack_list[arg.pack])
-                card.pack_id = inv_pack[card.id];
-            else
-                card.pack_id = null;
-        }
-        else {
-            card.pack_id = card.id % 1000;
-        }
-        ret.push(card);
-    }
-    stmt.free();
+		// pack_id
+		if (card.id <= 99999999) {
+			if (arg.pack && pack_list[arg.pack])
+				card.pack_id = inv_pack[card.id];
+			else
+				card.pack_id = null;
+		}
+		else {
+			card.pack_id = card.id % 1000;
+		}
+		ret.push(card);
+	}
+	stmt.free();
 }
 
 // query cards in main db and pre-release db
