8da8d3277cd8eee6a467dc57a7a5186a43c2875e
salix5 <salix5566@gmail.com>
2023-03-29 02:22:14 +0800

    update pack

--- query/pack.js
+++ query/pack.js
@@ -1,20 +1,16 @@
 "use strict";
-const last_pack = "DBWS#7";
+const last_pack = "DBWS#9";
 
 const pre_release={
 	VJMP: 100200001,
-	DBWS: 100420001,
 	DUNE: 101201001,
 	
 	// TCG
-	_MAZE: 100206001,
 };
 
 const wiki_link = {
 	VJMP: "https://yugioh-wiki.net/index.php?%BD%F1%C0%D2%C9%D5%C2%B0%A5%AB%A1%BC%A5%C9#VJ2023",
-	DBWS: "https://yugioh-wiki.net/index.php?%A5%C7%A5%C3%A5%AD%A5%D3%A5%EB%A5%C9%A5%D1%A5%C3%A5%AF%20%A5%EF%A5%A4%A5%EB%A5%C9%A1%A6%A5%B5%A5%D0%A5%A4%A5%D0%A1%BC%A5%BA",
 	DUNE: "https://yugioh-wiki.net/index.php?DUELIST%20NEXUS",
 	
 	// TCG
-	_MAZE: "https://yugipedia.com/wiki/Maze_of_Memories",
 };
