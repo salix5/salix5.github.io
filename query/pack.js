38571d11c594907129c3021a08106ea43e928978
salix5 <salix5566@gmail.com>
2023-04-07 15:50:37 +0800

    update pack

--- query/pack.js
+++ query/pack.js
@@ -4,6 +4,7 @@ const last_pack = "DBWS#10";
 const pre_release={
 	VJMP: 100200001,
 	DUNE: 101201001,
+	DP28: 100428001,
 	
 	// TCG
 };
@@ -11,6 +12,7 @@ const pre_release={
 const wiki_link = {
 	VJMP: "https://yugioh-wiki.net/index.php?%BD%F1%C0%D2%C9%D5%C2%B0%A5%AB%A1%BC%A5%C9#VJ2023",
 	DUNE: "https://yugioh-wiki.net/index.php?DUELIST%20NEXUS",
+	DP28: "https://yugioh-wiki.net/index.php?cmd=read&page=%A5%C7%A5%E5%A5%A8%A5%EA%A5%B9%A5%C8%A5%D1%A5%C3%A5%AF%A1%DD%C7%FA%B1%EA%A4%CE%A5%C7%A5%E5%A5%A8%A5%EA%A5%B9%A5%C8%CA%D4%A1%DD&word=dp28",
 	
 	// TCG
 };
