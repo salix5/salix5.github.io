c4ce50ad65f9bf9d6cbcc9ff7b16ed43d443c76a
salix5 <salix5566@gmail.com>
2023-04-13 14:20:59 +0800

    update pack

--- query/pack.js
+++ query/pack.js
@@ -6,6 +6,7 @@ const pre_release={
 	DUNE: 101201001,
 	DP28: 100428001,
 	AC03: 100207001,
+	SD46: 100346001,
 
 	// TCG
 };
@@ -15,6 +16,7 @@ const wiki_link = {
 	DUNE: "https://yugioh-wiki.net/index.php?DUELIST%20NEXUS",
 	DP28: "https://yugioh-wiki.net/index.php?cmd=read&page=%A5%C7%A5%E5%A5%A8%A5%EA%A5%B9%A5%C8%A5%D1%A5%C3%A5%AF%A1%DD%C7%FA%B1%EA%A4%CE%A5%C7%A5%E5%A5%A8%A5%EA%A5%B9%A5%C8%CA%D4%A1%DD&word=dp28",
 	AC03: "https://yugioh-wiki.net/index.php?ANIMATION%20CHRONICLE%202023",
+	SD46: "https://yugioh-wiki.net/index.php?%A5%B9%A5%C8%A5%E9%A5%AF%A5%C1%A5%E3%A1%BC%A5%C7%A5%C3%A5%AD%A1%DD%B2%A6%BC%D4%A4%CE%B8%DD%C6%B0%A1%DD",
 
 	// TCG
 };
