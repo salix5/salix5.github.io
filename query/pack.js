977b71ec879b58262dba3dfdcf0d5ccd8a1af5b3
salix5 <salix5566@gmail.com>
2023-06-03 01:59:21 +0800

    update pack list

--- query/pack.js
+++ query/pack.js
@@ -3,7 +3,6 @@ const pre_release={
 	VJMP: 100200001,
 	YOS1: 100296001,
 	
-	DUNE: 101201000,
 	DP28: 100428001,
 	AC03: 100207001,
 	SD46: 100346001,
@@ -17,7 +16,6 @@ const wiki_link = {
 	VJMP: "https://yugioh-wiki.net/index.php?%BD%F1%C0%D2%C9%D5%C2%B0%A5%AB%A1%BC%A5%C9#VJ2023",
 	YOS1: "https://yugioh-wiki.net/index.php?%BD%F1%C0%D2%C9%D5%C2%B0%A5%AB%A1%BC%A5%C9#YOS",
 	
-	DUNE: "https://yugioh-wiki.net/index.php?DUELIST%20NEXUS",
 	DP28: "https://yugioh-wiki.net/index.php?cmd=read&page=%A5%C7%A5%E5%A5%A8%A5%EA%A5%B9%A5%C8%A5%D1%A5%C3%A5%AF%A1%DD%C7%FA%B1%EA%A4%CE%A5%C7%A5%E5%A5%A8%A5%EA%A5%B9%A5%C8%CA%D4%A1%DD&word=dp28",
 	AC03: "https://yugioh-wiki.net/index.php?ANIMATION%20CHRONICLE%202023",
 	SD46: "https://yugioh-wiki.net/index.php?%A5%B9%A5%C8%A5%E9%A5%AF%A5%C1%A5%E3%A1%BC%A5%C7%A5%C3%A5%AD%A1%DD%B2%A6%BC%D4%A4%CE%B8%DD%C6%B0%A1%DD",
