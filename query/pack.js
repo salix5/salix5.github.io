c43efc7f58bfa43ef746880a78f14b7a9169b1ca
salix5 <salix5566@gmail.com>
2023-06-24 14:04:14 +0800

    update pack

--- query/pack.js
+++ query/pack.js
@@ -14,7 +14,6 @@ const pre_release={
 const wiki_link = {
 	VJMP: "https://yugioh-wiki.net/index.php?%BD%F1%C0%D2%C9%D5%C2%B0%A5%AB%A1%BC%A5%C9#VJ2023",
 	
-	SD46: "https://yugioh-wiki.net/index.php?%A5%B9%A5%C8%A5%E9%A5%AF%A5%C1%A5%E3%A1%BC%A5%C7%A5%C3%A5%AD%A1%DD%B2%A6%BC%D4%A4%CE%B8%DD%C6%B0%A1%DD",
 	AGOV: "https://yugioh-wiki.net/index.php?AGE%20OF%20OVERLORD",
 
 	// TCG
