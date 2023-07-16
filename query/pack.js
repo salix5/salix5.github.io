ba08b7c93ef6ed96dfbdca5e86d8feff0270f853
salix5 <salix5566@gmail.com>
2023-07-16 23:45:29 +0800

    update pack

--- query/pack.js
+++ query/pack.js
@@ -3,18 +3,20 @@ const pre_release={
 	VJMP: 100200001,
 	YOS1: 100296001,
 	
-	DP28: 100428001,
-	AC03: 100207001,
-	SD46: 100346001,
 	AGOV: 101202001,
+	DBVS: 100421001,
+	SR14: 100314001,
 
 	// TCG
 };
 
 const wiki_link = {
 	VJMP: "https://yugioh-wiki.net/index.php?%BD%F1%C0%D2%C9%D5%C2%B0%A5%AB%A1%BC%A5%C9#VJ2023",
+	YOS1: "https://yugioh-wiki.net/index.php?%BD%F1%C0%D2%C9%D5%C2%B0%A5%AB%A1%BC%A5%C9#YOS",
 	
 	AGOV: "https://yugioh-wiki.net/index.php?AGE%20OF%20OVERLORD",
+	DBVS: "https://yugioh-wiki.net/index.php?%A5%C7%A5%C3%A5%AD%A5%D3%A5%EB%A5%C9%A5%D1%A5%C3%A5%AF%20%A5%F4%A5%A1%A5%EA%A5%A2%A5%F3%A5%C8%A1%A6%A5%B9%A5%DE%A5%C3%A5%B7%A5%E3%A1%BC%A5%BA",
+	SR14: "https://yugioh-wiki.net/index.php?%A5%B9%A5%C8%A5%E9%A5%AF%A5%C1%A5%E3%A1%BC%A5%C7%A5%C3%A5%ADR%A1%DD%B1%EA%B2%A6%A4%CE%B5%DE%BD%B1%A1%DD",
 
 	// TCG
 };
