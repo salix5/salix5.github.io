2aba2aa845777db2e8de3d60a4982d7dd06cd065
salix5 <salix5566@gmail.com>
2023-08-11 18:56:45 +0800

    update pack

--- query/pack.js
+++ query/pack.js
@@ -6,6 +6,7 @@ const pre_release={
 	AGOV: 101202001,
 	DBVS: 100421001,
 	SR14: 100314001,
+	PHNI: 101203001,
 
 	// TCG
 	_DUNE: 101201001,
@@ -18,6 +19,7 @@ const wiki_link = {
 	AGOV: "https://yugioh-wiki.net/index.php?AGE%20OF%20OVERLORD",
 	DBVS: "https://yugioh-wiki.net/index.php?%A5%C7%A5%C3%A5%AD%A5%D3%A5%EB%A5%C9%A5%D1%A5%C3%A5%AF%20%A5%F4%A5%A1%A5%EA%A5%A2%A5%F3%A5%C8%A1%A6%A5%B9%A5%DE%A5%C3%A5%B7%A5%E3%A1%BC%A5%BA",
 	SR14: "https://yugioh-wiki.net/index.php?%A5%B9%A5%C8%A5%E9%A5%AF%A5%C1%A5%E3%A1%BC%A5%C7%A5%C3%A5%ADR%A1%DD%B1%EA%B2%A6%A4%CE%B5%DE%BD%B1%A1%DD",
+	PHNI: "https://yugioh-wiki.net/index.php?PHANTOM%20NIGHTMARE",
 
 	// TCG
 	_DUNE: "https://yugipedia.com/wiki/Duelist_Nexus",
