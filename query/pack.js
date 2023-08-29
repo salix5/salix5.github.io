21aff92157c18a350bf50b694ca4884ccf59583b
salix5 <salix5566@gmail.com>
2023-08-30 00:55:36 +0800

    update pack

--- query/pack.js
+++ query/pack.js
@@ -2,21 +2,25 @@
 const pre_release = {
 	VJMP: 100200001,
 	YOS1: 100296001,
+	YO07: 100208001,
 	
 	DBVS: 100421001,
 	SR14: 100314001,
 	PHNI: 101203001,
 
 	// TCG
+	MP23: 100209001,
 };
 
 const wiki_link = {
 	VJMP: "https://yugioh-wiki.net/index.php?%BD%F1%C0%D2%C9%D5%C2%B0%A5%AB%A1%BC%A5%C9#VJ2023",
 	YOS1: "https://yugioh-wiki.net/index.php?%BD%F1%C0%D2%C9%D5%C2%B0%A5%AB%A1%BC%A5%C9#YOS",
+	YO07: "https://yugioh-wiki.net/index.php?%BD%F1%C0%D2%C9%D5%C2%B0%A5%AB%A1%BC%A5%C9#YO",
 	
 	DBVS: "https://yugioh-wiki.net/index.php?%A5%C7%A5%C3%A5%AD%A5%D3%A5%EB%A5%C9%A5%D1%A5%C3%A5%AF%20%A5%F4%A5%A1%A5%EA%A5%A2%A5%F3%A5%C8%A1%A6%A5%B9%A5%DE%A5%C3%A5%B7%A5%E3%A1%BC%A5%BA",
 	SR14: "https://yugioh-wiki.net/index.php?%A5%B9%A5%C8%A5%E9%A5%AF%A5%C1%A5%E3%A1%BC%A5%C7%A5%C3%A5%ADR%A1%DD%B1%EA%B2%A6%A4%CE%B5%DE%BD%B1%A1%DD",
 	PHNI: "https://yugioh-wiki.net/index.php?PHANTOM%20NIGHTMARE",
 
 	// TCG
+	MP23: "https://yugipedia.com/wiki/25th_Anniversary_Tin:_Dueling_Heroes_Mega_Pack",
 };
