e635701877baeb734b970f781499c04fd1fe23b8
salix5 <salix5566@gmail.com>
2023-09-14 01:14:13 +0800

    update pack

--- query/pack.js
+++ query/pack.js
@@ -6,10 +6,10 @@ const pre_release = {
 	
 	DBVS: 100421001,
 	SR14: 100314001,
+	WPP4: 100209001,
 	PHNI: 101203001,
 
 	// TCG
-	MP23: 100209001,
 };
 
 const wiki_link = {
@@ -19,8 +19,8 @@ const wiki_link = {
 	
 	DBVS: "https://yugioh-wiki.net/index.php?%A5%C7%A5%C3%A5%AD%A5%D3%A5%EB%A5%C9%A5%D1%A5%C3%A5%AF%20%A5%F4%A5%A1%A5%EA%A5%A2%A5%F3%A5%C8%A1%A6%A5%B9%A5%DE%A5%C3%A5%B7%A5%E3%A1%BC%A5%BA",
 	SR14: "https://yugioh-wiki.net/index.php?%A5%B9%A5%C8%A5%E9%A5%AF%A5%C1%A5%E3%A1%BC%A5%C7%A5%C3%A5%ADR%A1%DD%B1%EA%B2%A6%A4%CE%B5%DE%BD%B1%A1%DD",
+	WPP4: "https://yugioh-wiki.net/index.php?WORLD%20PREMIERE%20PACK%202023",
 	PHNI: "https://yugioh-wiki.net/index.php?PHANTOM%20NIGHTMARE",
 
 	// TCG
-	MP23: "https://yugipedia.com/wiki/25th_Anniversary_Tin:_Dueling_Heroes_Mega_Pack",
 };
