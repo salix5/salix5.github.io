93208218391c5936c917320057181bc90a96caef
salix5 <salix5566@gmail.com>
2023-10-03 18:28:05 +0800

    ygo-query: update print_data()

--- query/ygo-query.js
+++ query/ygo-query.js
@@ -170,28 +170,28 @@ function print_data(card, newline) {
 	else if (card.type & TYPE_SPELL) {
 		mtype = `${type_name[TYPE_SPELL]}`;
 		if (card.type & TYPE_QUICKPLAY)
-			subtype = `${type_name[TYPE_QUICKPLAY]}`;
+			subtype = `/${type_name[TYPE_QUICKPLAY]}`;
 		else if (card.type & TYPE_CONTINUOUS)
-			subtype = `${type_name[TYPE_CONTINUOUS]}`;
+			subtype = `/${type_name[TYPE_CONTINUOUS]}`;
 		else if (card.type & TYPE_EQUIP)
-			subtype = `${type_name[TYPE_EQUIP]}`;
+			subtype = `/${type_name[TYPE_EQUIP]}`;
 		else if (card.type & TYPE_RITUAL)
-			subtype = `${type_name[TYPE_RITUAL]}`;
+			subtype = `/${type_name[TYPE_RITUAL]}`;
 		else if (card.type & TYPE_FIELD)
-			subtype = `${type_name[TYPE_FIELD]}`;
+			subtype = `/${type_name[TYPE_FIELD]}`;
 		else
-			subtype = `${type_name[TYPE_NORMAL]}`;
-		data = `[${subtype}${mtype}]${newline}`;
+			subtype = `/${type_name[TYPE_NORMAL]}`;
+		data = `[${mtype}${subtype}]${newline}`;
 	}
 	else if (card.type & TYPE_TRAP) {
 		mtype = `${type_name[TYPE_TRAP]}`;
 		if (card.type & TYPE_CONTINUOUS)
-			subtype = `${type_name[TYPE_CONTINUOUS]}`;
+			subtype = `/${type_name[TYPE_CONTINUOUS]}`;
 		else if (card.type & TYPE_COUNTER)
-			subtype = `${type_name[TYPE_COUNTER]}`;
+			subtype = `/${type_name[TYPE_COUNTER]}`;
 		else
-			subtype = `${type_name[TYPE_NORMAL]}`;
-		data = `[${subtype}${mtype}]${newline}`;
+			subtype = `/${type_name[TYPE_NORMAL]}`;
+		data = `[${mtype}${subtype}]${newline}`;
 	}
 	return data;
 }
