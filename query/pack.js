66e4ab831b51631fec1d92e38a9091e88bc1304c
salix5 <salix5566@gmail.com>
2023-12-11 12:08:10 +0800

    update pack

--- query/pack.js
+++ query/pack.js
@@ -2,6 +2,7 @@
 const pre_release = {
 	VJMP: 100200001,
 	YOS1: 100215001,
+	YO08: 100216001,
 
 	PHNI: 101203001,
 	TW01: 100211001,
@@ -17,6 +18,7 @@ const pre_release = {
 const wiki_link = {
 	VJMP: "https://yugioh-wiki.net/index.php?%BD%F1%C0%D2%C9%D5%C2%B0%A5%AB%A1%BC%A5%C9#VJ2023",
 	YOS1: "https://yugioh-wiki.net/index.php?%BD%F1%C0%D2%C9%D5%C2%B0%A5%AB%A1%BC%A5%C9#YOS",
+	YO08: "https://yugioh-wiki.net/index.php?%BD%F1%C0%D2%C9%D5%C2%B0%A5%AB%A1%BC%A5%C9#YO",
 
 	PHNI: "https://yugioh-wiki.net/index.php?PHANTOM%20NIGHTMARE",
 	TW01: "https://yugioh-wiki.net/index.php?TERMINAL%20WORLD",
