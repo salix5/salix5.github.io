2db49bb27361165c6cc6a08a9941854983e08c29
salix5 <salix5566@gmail.com>
2023-12-20 14:43:13 +0800

    update pack

--- query/pack.js
+++ query/pack.js
@@ -4,15 +4,12 @@ const pre_release = {
 	YOS1: 100215001,
 	YO08: 100216001,
 
-	PHNI: 101203001,
-	TW01: 100211001,
 	QCDB: 100212001,
 	"24PR": 100213001,
 	"24PP": 100214001,
 	LEDE: 101204001,
 
 	// TCG
-	_AGOV: 101202001,
 };
 
 const wiki_link = {
@@ -20,13 +17,10 @@ const wiki_link = {
 	YOS1: "https://yugioh-wiki.net/index.php?%BD%F1%C0%D2%C9%D5%C2%B0%A5%AB%A1%BC%A5%C9#YOS",
 	YO08: "https://yugioh-wiki.net/index.php?%BD%F1%C0%D2%C9%D5%C2%B0%A5%AB%A1%BC%A5%C9#YO",
 
-	PHNI: "https://yugioh-wiki.net/index.php?PHANTOM%20NIGHTMARE",
-	TW01: "https://yugioh-wiki.net/index.php?TERMINAL%20WORLD",
 	QCDB: "https://yugioh-wiki.net/index.php?QUARTER%20CENTURY%20DUELIST%20BOX",
 	"24PR": "https://yugioh-wiki.net/index.php?%A5%D7%A5%ED%A5%E2%A5%AB%A1%BC%A5%C9#JF24",
 	"24PP": "https://yugioh-wiki.net/index.php?PREMIUM%20PACK#PP2024",
 	LEDE: "https://yugioh-wiki.net/index.php?LEGACY%20OF%20DESTRUCTION",
 
 	// TCG
-	_AGOV: "https://yugipedia.com/wiki/Age_of_Overlord",
 };
