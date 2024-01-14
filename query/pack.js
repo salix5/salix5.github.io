5fc089918099b5da302cde9f8d1a6089bd21e622
salix5 <salix5566@gmail.com>
2024-01-14 23:06:47 +0800

    update pack

--- query/pack.js
+++ query/pack.js
@@ -4,12 +4,11 @@ const pre_release = {
 	YOS1: 100215001,
 	YO08: 100216001,
 
-	QCDB: 100212001,
-	"24PR": 100213001,
-	"24PP": 100214001,
 	LEDE: 101204001,
+	TDPR: 100218001,
 
 	// TCG
+	MZMI: 100217001,
 };
 
 const wiki_link = {
@@ -17,10 +16,9 @@ const wiki_link = {
 	YOS1: "https://yugioh-wiki.net/index.php?%BD%F1%C0%D2%C9%D5%C2%B0%A5%AB%A1%BC%A5%C9#YOS",
 	YO08: "https://yugioh-wiki.net/index.php?%BD%F1%C0%D2%C9%D5%C2%B0%A5%AB%A1%BC%A5%C9#YO",
 
-	QCDB: "https://yugioh-wiki.net/index.php?QUARTER%20CENTURY%20DUELIST%20BOX",
-	"24PR": "https://yugioh-wiki.net/index.php?%A5%D7%A5%ED%A5%E2%A5%AB%A1%BC%A5%C9#JF24",
-	"24PP": "https://yugioh-wiki.net/index.php?PREMIUM%20PACK#PP2024",
 	LEDE: "https://yugioh-wiki.net/index.php?LEGACY%20OF%20DESTRUCTION",
+	TDPR: "https://yugioh-wiki.net/index.php?%A5%D7%A5%ED%A5%E2%A5%AB%A1%BC%A5%C9#LEGEND_PROMO",
 
 	// TCG
+	MZMI: "https://yugipedia.com/wiki/Maze_of_Millennia",
 };
