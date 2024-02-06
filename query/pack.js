94e6d5d285ba18fd8d6474a634656f00bf64200e
salix5 <salix5566@gmail.com>
2024-02-06 10:48:31 +0800

    Update pack.js

--- query/pack.js
+++ query/pack.js
@@ -4,8 +4,7 @@ const pre_release = {
 	YOS1: 100296001,
 	YO08: 100216001,
 
-	LEDE: 101204001,
-	INFO: 101205001,
+	INFO: 101205000,
 	TDPR: 100017001,
 
 	// TCG
@@ -18,7 +17,6 @@ const wiki_link = {
 	YOS1: "https://yugioh-wiki.net/index.php?%BD%F1%C0%D2%C9%D5%C2%B0%A5%AB%A1%BC%A5%C9#YOS",
 	YO08: "https://yugioh-wiki.net/index.php?%BD%F1%C0%D2%C9%D5%C2%B0%A5%AB%A1%BC%A5%C9#YO",
 
-	LEDE: "https://yugioh-wiki.net/index.php?LEGACY%20OF%20DESTRUCTION",
 	INFO: "https://yugioh-wiki.net/index.php?INFINITE%20FORBIDDEN",
 	TDPR: "https://yugioh-wiki.net/index.php?%A5%D7%A5%ED%A5%E2%A5%AB%A1%BC%A5%C9#LEGEND_PROMO",
 
