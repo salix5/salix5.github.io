0dc10d2367c1510bd641cd46085660eed8716922
salix5 <salix5566@gmail.com>
2024-02-27 17:35:02 +0800

    update pack

--- query/pack.js
+++ query/pack.js
@@ -3,18 +3,20 @@ const pre_release = {
 	VJMP: 100200001,
 	YOS1: 100296001,
 
+	YO08: 100216001,
+	VX04: 100219001,
 	INFO: 101205000,
 
 	// TCG
-	_PHNI: 101203001,
 };
 
 const wiki_link = {
-	VJMP: "https://yugioh-wiki.net/index.php?%BD%F1%C0%D2%C9%D5%C2%B0%A5%AB%A1%BC%A5%C9#VJ2023",
+	VJMP: "https://yugioh-wiki.net/index.php?%BD%F1%C0%D2%C9%D5%C2%B0%A5%AB%A1%BC%A5%C9#VJ2024",
 	YOS1: "https://yugioh-wiki.net/index.php?%BD%F1%C0%D2%C9%D5%C2%B0%A5%AB%A1%BC%A5%C9#YOS",
 
+	YO08: "https://yugioh-wiki.net/index.php?%BD%F1%C0%D2%C9%D5%C2%B0%A5%AB%A1%BC%A5%C9#YO",
+	VX04: "https://yugioh-wiki.net/index.php?%BD%F1%C0%D2%C9%D5%C2%B0%A5%AB%A1%BC%A5%C9#VX04",
 	INFO: "https://yugioh-wiki.net/index.php?INFINITE%20FORBIDDEN",
 
 	// TCG
-	_PHNI: "https://yugipedia.com/wiki/Phantom_Nightmare",
 };
