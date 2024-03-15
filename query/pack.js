ac61446445fdfdcf51246398273dc0a793435168
salix5 <salix5566@gmail.com>
2024-03-16 01:39:25 +0800

    update pack

--- query/pack.js
+++ query/pack.js
@@ -6,6 +6,7 @@ const pre_release = {
 	YO08: 100215001,
 	VX04: 100219001,
 	INFO: 101205000,
+	DP29: 100220001,
 
 	// TCG
 };
@@ -17,6 +18,7 @@ const wiki_link = {
 	YO08: "https://yugioh-wiki.net/index.php?%BD%F1%C0%D2%C9%D5%C2%B0%A5%AB%A1%BC%A5%C9#YO",
 	VX04: "https://yugioh-wiki.net/index.php?%BD%F1%C0%D2%C9%D5%C2%B0%A5%AB%A1%BC%A5%C9#VX04",
 	INFO: "https://yugioh-wiki.net/index.php?INFINITE%20FORBIDDEN",
+	DP29: "https://yugioh-wiki.net/index.php?%A5%C7%A5%E5%A5%A8%A5%EA%A5%B9%A5%C8%A5%D1%A5%C3%A5%AF%A1%DD%B5%B1%B8%F7%A4%CE%A5%C7%A5%E5%A5%A8%A5%EA%A5%B9%A5%C8%CA%D4%A1%DD",
 
 	// TCG
 };
