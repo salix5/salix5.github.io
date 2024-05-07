9a0ff2a7885715bd28cf9e57b8ec626f88764d10
salix5 <salix5566@gmail.com>
2024-05-08 05:09:13 +0800

    update pack

--- query/pack.js
+++ query/pack.js
@@ -1,14 +1,12 @@
 "use strict";
 const pre_release = {
-	VJMP: 100200001,
-	YOS1: 100296001,
+	VJMP: 100200000,
+	YOS1: 100296000,
 
-	YO08: 100215001,
-	VX04: 100219001,
 	INFO: 101205000,
-	DP29: 100220001,
+	DP29: 100220000,
 	CF01: 100221000,
-	AC04: 100222001,
+	AC04: 100222000,
 
 	// TCG
 	_LEDE: 101204001,
@@ -18,8 +16,6 @@ const wiki_link = {
 	VJMP: "https://yugioh-wiki.net/index.php?%BD%F1%C0%D2%C9%D5%C2%B0%A5%AB%A1%BC%A5%C9#VJ2024",
 	YOS1: "https://yugioh-wiki.net/index.php?%BD%F1%C0%D2%C9%D5%C2%B0%A5%AB%A1%BC%A5%C9#YOS",
 
-	YO08: "https://yugioh-wiki.net/index.php?%BD%F1%C0%D2%C9%D5%C2%B0%A5%AB%A1%BC%A5%C9#YO",
-	VX04: "https://yugioh-wiki.net/index.php?%BD%F1%C0%D2%C9%D5%C2%B0%A5%AB%A1%BC%A5%C9#VX04",
 	INFO: "https://yugioh-wiki.net/index.php?INFINITE%20FORBIDDEN",
 	DP29: "https://yugioh-wiki.net/index.php?%A5%C7%A5%E5%A5%A8%A5%EA%A5%B9%A5%C8%A5%D1%A5%C3%A5%AF%A1%DD%B5%B1%B8%F7%A4%CE%A5%C7%A5%E5%A5%A8%A5%EA%A5%B9%A5%C8%CA%D4%A1%DD",
 	CF01: "https://yugioh-wiki.net/index.php?COMPLETE%20FILE%20%A1%DD%C7%F2%A4%CE%CA%AA%B8%EC%A1%DD",
