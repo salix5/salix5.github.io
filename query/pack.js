5962790065f22304416650c06d1848037f12b4e3
salix5 <salix5566@gmail.com>
2024-06-26 22:47:50 +0800

    update pack

--- query/pack.js
+++ query/pack.js
@@ -7,6 +7,7 @@ const pre_release = {
 	AC04: 100222000,
 	ROTA: 101206000,
 	DBCB: 100422000,
+	"24TP": 100230001,
 
 	// TCG
 };
@@ -19,6 +20,7 @@ const wiki_link = {
 	AC04: "https://yugioh-wiki.net/index.php?ANIMATION%20CHRONICLE%202024",
 	ROTA: "https://yugioh-wiki.net/index.php?RAGE%20OF%20THE%20ABYSS",
 	DBCB: "https://yugioh-wiki.net/index.php?%A5%C7%A5%C3%A5%AD%A5%D3%A5%EB%A5%C9%A5%D1%A5%C3%A5%AF%20%A5%AF%A5%ED%A5%B9%A5%AA%A1%BC%A5%D0%A1%BC%A1%A6%A5%D6%A5%EC%A5%A4%A5%AB%A1%BC%A5%BA",
+	"24TP": "https://yugioh-wiki.net/index.php?%A5%C8%A1%BC%A5%CA%A5%E1%A5%F3%A5%C8%A5%D1%A5%C3%A5%AF2024%20Vol.3",
 
 	// TCG
 };
