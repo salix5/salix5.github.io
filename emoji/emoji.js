52d670f057ffecfd0a916439bc9b282d093f277d
salix5 <salix5566@gmail.com>
2024-07-05 23:00:49 +0800

    emoji: accept snowflake

--- emoji/emoji.js
+++ emoji/emoji.js
@@ -2,15 +2,34 @@
 const text1 = document.getElementById('text1');
 const div_link = document.getElementById('div_link');
 const div_date = document.getElementById('div_date');
-const re_id = /<(a?):[^:]+:(\d+)>/
+const re_emoji = /<(a?):[^:]+:(\d+)>/;
+const re_snowflake = /^\d+$/;
 
 text1.value = '';
 button1.onclick = create_url;
+
+/**
+ * @param {string} id 
+ * @returns 
+ */
+function get_discord_timestamp(id) {
+	if (!window.BigInt)
+		return -1;
+	const snowflake = BigInt(id);
+	const offset = BigInt(22);
+	const discord_epoch = BigInt(1420070400000);
+	const timestamp = Number((snowflake >> offset) + discord_epoch);
+	return timestamp;
+}
+
 function create_url(e) {
-	const result = text1.value.match(re_id);
-	text1.value = '';
+	const result = text1.value.match(re_emoji);
+	let timestamp = -1;
 	div_link.innerHTML = '';
-	if (result) {
+	if (re_snowflake.test(text1.value)) {
+		timestamp = get_discord_timestamp(text1.value);
+	}
+	else if (result) {
 		const id = result[2];
 		const ext = (result[1] === 'a') ? 'gif' : 'webp';
 		const size = 64;
@@ -21,13 +40,11 @@ function create_url(e) {
 		link1.rel = 'noreferrer nofollow';
 		link1.textContent = url;
 		div_link.appendChild(link1);
-		if (window.BigInt) {
-			const snowflake = BigInt(id);
-			const offset = BigInt(22);
-			const base = BigInt(1420070400000);
-			const timestamp = Number((snowflake >> offset) + base);
-			const birth = new Date(timestamp);
-			div_date.textContent = `生於：${birth.toLocaleString()}`;
-		}
+		timestamp = get_discord_timestamp(id);
 	}
+	if (timestamp > 0) {
+		const birth = new Date(timestamp);
+		div_date.textContent = `時間：${birth.toLocaleString()}`;
+	}
+	text1.value = '';
 }
