22e7dac200a8b00909739f2d89bf339e322f7587
salix5 <salix5566@gmail.com>
2024-07-11 12:59:28 +0800

    update pack

--- query/pack.js
+++ query/pack.js
@@ -5,9 +5,11 @@ const pre_release = {
 
 	CF01: 100221000,
 	ROTA: 101206000,
-	DBCB: 100422000,
+	DBCB: 100223000,
+	SD47: 100347000,
 
 	// TCG
+	_INFO: 101205000,
 };
 
 const wiki_link = {
@@ -17,6 +19,8 @@ const wiki_link = {
 	CF01: "https://yugioh-wiki.net/index.php?COMPLETE%20FILE%20%A1%DD%C7%F2%A4%CE%CA%AA%B8%EC%A1%DD",
 	ROTA: "https://yugioh-wiki.net/index.php?RAGE%20OF%20THE%20ABYSS",
 	DBCB: "https://yugioh-wiki.net/index.php?%A5%C7%A5%C3%A5%AD%A5%D3%A5%EB%A5%C9%A5%D1%A5%C3%A5%AF%20%A5%AF%A5%ED%A5%B9%A5%AA%A1%BC%A5%D0%A1%BC%A1%A6%A5%D6%A5%EC%A5%A4%A5%AB%A1%BC%A5%BA",
+	SD47: "https://yugioh-wiki.net/index.php?%A5%B9%A5%C8%A5%E9%A5%AF%A5%C1%A5%E3%A1%BC%A5%C7%A5%C3%A5%AD%A1%DD%C0%C4%A4%AD%B4%E3%A4%CE%B8%F7%CE%D7%A1%DD",
 
 	// TCG
+	_INFO: "https://yugipedia.com/wiki/The_Infinite_Forbidden",
 };
