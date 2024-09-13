5e226c6d317493c45d68ecbdf232c6c59224bb5b
salix5 <salix5566@gmail.com>
2024-09-13 09:44:54 +0800

    update pack

--- query/pack.js
+++ query/pack.js
@@ -7,11 +7,12 @@ const pre_release = {
 	DBCB: 100223000,
 	SD47: 100224000,
 	YO09: 100225000,
-	WPP5: 100226000,
+	TW02: 100227000,
 	SUDA: 101207000,
 
 	// TCG
 	_ROTA: 101206000,
+	MP24: 100226000,
 };
 
 const wiki_link = {
@@ -22,9 +23,10 @@ const wiki_link = {
 	DBCB: "https://yugioh-wiki.net/index.php?%A5%C7%A5%C3%A5%AD%A5%D3%A5%EB%A5%C9%A5%D1%A5%C3%A5%AF%20%A5%AF%A5%ED%A5%B9%A5%AA%A1%BC%A5%D0%A1%BC%A1%A6%A5%D6%A5%EC%A5%A4%A5%AB%A1%BC%A5%BA",
 	SD47: "https://yugioh-wiki.net/index.php?%A5%B9%A5%C8%A5%E9%A5%AF%A5%C1%A5%E3%A1%BC%A5%C7%A5%C3%A5%AD%A1%DD%C0%C4%A4%AD%B4%E3%A4%CE%B8%F7%CE%D7%A1%DD",
 	YO09: "https://yugioh-wiki.net/index.php?%BD%F1%C0%D2%C9%D5%C2%B0%A5%AB%A1%BC%A5%C9#YO",
-	WPP5: "https://yugioh-wiki.net/index.php?WORLD%20PREMIERE%20PACK%202024",
+	TW02: "https://yugioh-wiki.net/index.php?TERMINAL%20WORLD%202",
 	SUDA: "https://yugioh-wiki.net/index.php?SUPREME%20DARKNESS",
 
 	// TCG
 	_ROTA: "https://yugipedia.com/wiki/Rage_of_the_Abyss",
+	MP24: "https://yugipedia.com/wiki/25th_Anniversary_Tin:_Dueling_Mirrors",
 };
