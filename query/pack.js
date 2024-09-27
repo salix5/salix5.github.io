6a74b73c844b3375281de02f1a5fd9e55dd0155e
salix5 <salix5566@gmail.com>
2024-09-28 00:56:56 +0800

    24TP

--- query/pack.js
+++ query/pack.js
@@ -9,6 +9,7 @@ const pre_release = {
 	YO09: 100225000,
 	TW02: 100227000,
 	SUDA: 101207000,
+	"24TP": 100230000,
 
 	// TCG
 	_ROTA: 101206000,
@@ -24,6 +25,7 @@ const wiki_link = {
 	YO09: "https://yugioh-wiki.net/index.php?%BD%F1%C0%D2%C9%D5%C2%B0%A5%AB%A1%BC%A5%C9#YO",
 	TW02: "https://yugioh-wiki.net/index.php?TERMINAL%20WORLD%202",
 	SUDA: "https://yugioh-wiki.net/index.php?SUPREME%20DARKNESS",
+	"24TP": "https://yugioh-wiki.net/index.php?%A5%C8%A1%BC%A5%CA%A5%E1%A5%F3%A5%C8%A5%D1%A5%C3%A5%AF2024%20Vol.4",
 
 	// TCG
 	_ROTA: "https://yugipedia.com/wiki/Rage_of_the_Abyss",
