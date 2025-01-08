0204270871527b8adb182ab97fc56d7d82af20a4
salix5 <salix5566@gmail.com>
2025-01-08 21:48:40 +0800

    query: update wiki_link

--- query/pack.js
+++ query/pack.js
@@ -12,7 +12,7 @@ const pre_release = {
 };
 
 const wiki_link = {
-	VJMP: "https://yugioh-wiki.net/index.php?%BD%F1%C0%D2%C9%D5%C2%B0%A5%AB%A1%BC%A5%C9#VJ2024",
+	VJMP: "https://yugioh-wiki.net/index.php?%BD%F1%C0%D2%C9%D5%C2%B0%A5%AB%A1%BC%A5%C9#VJ2025",
 	YOS2: "https://yugioh-wiki.net/index.php?%BD%F1%C0%D2%C9%D5%C2%B0%A5%AB%A1%BC%A5%C9#YOS",
 
 	QCTB: "https://yugioh-wiki.net/index.php?QUARTER%20CENTURY%20TRINITY%20BOX",
