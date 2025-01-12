8d10c7b65300d1f2d9378791a9c7f8fd27a81378
salix5 <salix5566@gmail.com>
2025-01-12 21:57:55 +0800

    update pack

--- query/pack.js
+++ query/pack.js
@@ -3,22 +3,18 @@ const pre_release = {
 	VJMP: 100200000,
 	YOS2: 100228000,
 
-	QCTB: 100229000,
 	ALIN: 101208000,
-	"25PR": 100232000,
-	"25PP": 100233000,
 
 	// TCG
+	_SUDA: 101207000,
 };
 
 const wiki_link = {
 	VJMP: "https://yugioh-wiki.net/index.php?%BD%F1%C0%D2%C9%D5%C2%B0%A5%AB%A1%BC%A5%C9#VJ2025",
 	YOS2: "https://yugioh-wiki.net/index.php?%BD%F1%C0%D2%C9%D5%C2%B0%A5%AB%A1%BC%A5%C9#YOS",
 
-	QCTB: "https://yugioh-wiki.net/index.php?QUARTER%20CENTURY%20TRINITY%20BOX",
 	ALIN: "https://yugioh-wiki.net/index.php?ALLIANCE%20INSIGHT",
-	"25PR": "https://yugioh-wiki.net/index.php?%A5%D7%A5%ED%A5%E2%A5%AB%A1%BC%A5%C9#JF25",
-	"25PP": "https://yugioh-wiki.net/index.php?PREMIUM%20PACK#PP2025",
 
 	// TCG
+	_SUDA: "https://yugipedia.com/wiki/Supreme_Darkness",
 };
