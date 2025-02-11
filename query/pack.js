552e9de2117811f1a59329631b745f94da412388
salix5 <salix5566@gmail.com>
2025-02-11 23:37:17 +0800

    update pack

--- query/pack.js
+++ query/pack.js
@@ -4,6 +4,9 @@ const pre_release = {
 	YOS2: 100228000,
 
 	ALIN: 101208000,
+	YO10: 100234000,
+	VX05: 100235000,
+	DBJH: 100236000,
 
 	// TCG
 	_SUDA: 101207000,
@@ -14,6 +17,9 @@ const wiki_link = {
 	YOS2: "https://yugioh-wiki.net/index.php?%BD%F1%C0%D2%C9%D5%C2%B0%A5%AB%A1%BC%A5%C9#YOS",
 
 	ALIN: "https://yugioh-wiki.net/index.php?ALLIANCE%20INSIGHT",
+	YO10: "https://yugioh-wiki.net/index.php?%BD%F1%C0%D2%C9%D5%C2%B0%A5%AB%A1%BC%A5%C9#YO",
+	VX05: "https://yugioh-wiki.net/index.php?%BD%F1%C0%D2%C9%D5%C2%B0%A5%AB%A1%BC%A5%C9#VB",
+	DBJH: "https://yugioh-wiki.net/index.php?%A5%C7%A5%C3%A5%AD%A5%D3%A5%EB%A5%C9%A5%D1%A5%C3%A5%AF%20%A5%B8%A5%E3%A5%B9%A5%C6%A5%A3%A5%B9%A1%A6%A5%CF%A5%F3%A5%BF%A1%BC%A5%BA",
 
 	// TCG
 	_SUDA: "https://yugipedia.com/wiki/Supreme_Darkness",
