bb92d1b5168a2f4aabc0778d97ec9b53a4b5e58f
salix5 <salix5566@gmail.com>
2025-03-15 00:41:29 +0800

    update wiki link

--- query/pack.js
+++ query/pack.js
@@ -9,7 +9,6 @@ const pre_release = {
 	DUAD: 101301000,
 
 	// TCG
-	_SUDA: 101207000,
 	MZTM: 100237000,
 };
 
@@ -19,10 +18,9 @@ const wiki_link = {
 
 	YO10: "https://yugioh-wiki.net/index.php?%BD%F1%C0%D2%C9%D5%C2%B0%A5%AB%A1%BC%A5%C9#YO",
 	VX05: "https://yugioh-wiki.net/index.php?%BD%F1%C0%D2%C9%D5%C2%B0%A5%AB%A1%BC%A5%C9#VB",
-	DBJH: "https://yugioh-wiki.net/index.php?%A5%C7%A5%C3%A5%AD%A5%D3%A5%EB%A5%C9%A5%D1%A5%C3%A5%AF%20%A5%B8%A5%E3%A5%B9%A5%C6%A5%A3%A5%B9%A1%A6%A5%CF%A5%F3%A5%BF%A1%BC%A5%BA",
-	DUAD: "https://yugioh-wiki.net/index.php?DUELIST%20ADVANCE",
+	DBJH: "https://yugioh-wiki.net/index.php?%A5%C7%A5%C3%A5%AD%A5%D3%A5%EB%A5%C9%A5%D1%A5%C3%A5%AF%20%A5%B8%A5%E3%A5%B9%A5%C6%A5%A3%A5%B9%A1%A6%A5%CF%A5%F3%A5%BF%A1%BC%A5%BA#card",
+	DUAD: "https://yugioh-wiki.net/index.php?DUELIST%20ADVANCE#card",
 
 	// TCG
-	_SUDA: "https://yugipedia.com/wiki/Supreme_Darkness",
 	MZTM: "https://yugipedia.com/wiki/Maze_of_the_Master",
 };
