0273cd3748ec7f5c29e97a9acc3235ba69acfdfb
salix5 <salix5566@gmail.com>
2025-03-30 18:10:11 +0800

    minor update

--- query/event_handler.js
+++ query/event_handler.js
@@ -56,8 +56,7 @@ const select_page = document.getElementById("select_page");
 const table_result = document.getElementById("table_result");
 
 function clear_cb(name) {
-	let cb_list = document.getElementsByName(name);
-	for (const cb of cb_list) {
+	for (const cb of document.getElementsByName(name)) {
 		cb.checked = false;
 	}
 }
