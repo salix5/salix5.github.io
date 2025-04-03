4204bd04f7d8d0c557e3a6b5513c0b07319ea5c5
salix5 <salix5566@gmail.com>
2025-04-03 11:01:06 +0800

    Update pack.js

--- query/pack.js
+++ query/pack.js
@@ -4,12 +4,9 @@ const pre_release = {
 	YOS2: 100228000,
 
 	YO10: 100234000,
-	VX05: 100235000,
-	DBJH: 100236000,
 	DUAD: 101301000,
 
 	// TCG
-	MZTM: 100237000,
 };
 
 const wiki_link = {
@@ -17,10 +14,7 @@ const wiki_link = {
 	YOS2: "https://yugioh-wiki.net/index.php?%BD%F1%C0%D2%C9%D5%C2%B0%A5%AB%A1%BC%A5%C9#YOS",
 
 	YO10: "https://yugioh-wiki.net/index.php?%BD%F1%C0%D2%C9%D5%C2%B0%A5%AB%A1%BC%A5%C9#YO",
-	VX05: "https://yugioh-wiki.net/index.php?%BD%F1%C0%D2%C9%D5%C2%B0%A5%AB%A1%BC%A5%C9#VB",
-	DBJH: "https://yugioh-wiki.net/index.php?%A5%C7%A5%C3%A5%AD%A5%D3%A5%EB%A5%C9%A5%D1%A5%C3%A5%AF%20%A5%B8%A5%E3%A5%B9%A5%C6%A5%A3%A5%B9%A1%A6%A5%CF%A5%F3%A5%BF%A1%BC%A5%BA#card",
 	DUAD: "https://yugioh-wiki.net/index.php?DUELIST%20ADVANCE#card",
 
 	// TCG
-	MZTM: "https://yugipedia.com/wiki/Maze_of_the_Master",
 };
