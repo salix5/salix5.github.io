7f703992c616ff06ae7407340dc22153d782422c
salix5 <salix5566@gmail.com>
2025-04-03 11:40:25 +0800

    Update pack.js

--- query/pack.js
+++ query/pack.js
@@ -5,6 +5,7 @@ const pre_release = {
 
 	YO10: 100234000,
 	DUAD: 101301000,
+	"25YC": 100239000,
 
 	// TCG
 };
@@ -15,6 +16,7 @@ const wiki_link = {
 
 	YO10: "https://yugioh-wiki.net/index.php?%BD%F1%C0%D2%C9%D5%C2%B0%A5%AB%A1%BC%A5%C9#YO",
 	DUAD: "https://yugioh-wiki.net/index.php?DUELIST%20ADVANCE#card",
+	"25YC": "https://yugioh-wiki.net/index.php?%A5%D7%A5%ED%A5%E2%A5%AB%A1%BC%A5%C9#YCSJ2025",
 
 	// TCG
 };
