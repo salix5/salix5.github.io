9826bf389451b3fe51b4f0fdc6a35fed9d1f5ec9
salix5 <salix5566@gmail.com>
2025-05-09 20:24:08 +0800

    update pack

--- query/pack.js
+++ query/pack.js
@@ -6,9 +6,10 @@ const pre_release = {
 	YO10: 100234000,
 	DUAD: 101301000,
 	LPG1: 100240000,
+	VP25: 100241000,
+	TTP1: 100242000,
 
 	// TCG
-	_ALIN: 101208000,
 };
 
 const wiki_link = {
@@ -18,7 +19,8 @@ const wiki_link = {
 	YO10: "https://yugioh-wiki.net/index.php?%BD%F1%C0%D2%C9%D5%C2%B0%A5%AB%A1%BC%A5%C9#YO",
 	DUAD: "https://yugioh-wiki.net/index.php?DUELIST%20ADVANCE#card",
 	LPG1: "https://yugioh-wiki.net/index.php?LIMITED%20PACK%20GX%20%A1%DD%A5%AA%A5%B7%A5%EA%A5%B9%A5%EC%A5%C3%A5%C9%A1%DD#card",
+	VP25: "https://yugioh-wiki.net/index.php?V%20JUMP%20EDITION#VP25A",
+	TTP1: "https://yugioh-wiki.net/index.php?TACTICAL-TRY%20PACK%20%A1%DD%B9%F5%CB%E2%C6%B3%A1%A6%A3%C8%A3%C5%A3%D2%A3%CF%A1%A6%B8%E6%D6%E0%A1%DD#card",
 
 	// TCG
-	_ALIN: "https://yugipedia.com/wiki/Alliance_Insight",
 };
