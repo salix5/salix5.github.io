5f00a81eb592ec5bf35f352a4cce642ab7e3d1a3
salix5 <salix5566@gmail.com>
2026-01-10 23:49:18 +0800

    Refactor data loading in ygo-json-loader.js

--- query/ygo-json-loader.js
+++ query/ygo-json-loader.js
@@ -1,5 +1,5 @@
 "use strict";
-const last_pack = "26PP#6";
+const last_pack = "YOS3#1";
 const unknown_index = {
 };
 
@@ -47,33 +47,32 @@ let cid_table = null
 const name_table = Object.create(null);
 const md_table = Object.create(null);
 const complete_name_table = Object.create(null);
+const setname = Object.create(null);
+const ltable_md = Object.create(null);
+const cid_object = Object.create(null);
 const replace_name = Object.create(null);
 const pre_release = Object.create(null);
 const wiki_link = Object.create(null);
-
-const setname = Object.create(null);
-const ltable_md = Object.create(null);
-const md_card_list = Object.create(null);
 fetch_list.push(fetch(`text/md_name_jp.json`).then(response => response.json()).then(data => { md_table['ja'] = object_to_map(data) }));
 fetch_list.push(fetch(`text/md_name_en.json`).then(response => response.json()).then(data => { md_table['en'] = object_to_map(data) }));
 fetch_list.push(fetch(`text/setname.json`).then(response => response.json()).then(data => Object.assign(setname, data)));
 fetch_list.push(fetch(`text/lflist_md.json`).then(response => response.json()).then(data => Object.assign(ltable_md, data)));
-fetch_list.push(fetch(`text/CardList.json`).then(response => response.json()).then(data => Object.assign(md_card_list, data)));
+fetch_list.push(fetch(`text/cid_table.json`).then(response => response.json()).then(data => Object.assign(cid_object, data)));
 fetch_list.push(fetch(`text/replace_name.json`).then(response => response.json()).then(data => Object.assign(replace_name, data)));
 fetch_list.push(fetch(`pack/pre_release.json`).then(response => response.json()).then(data => Object.assign(pre_release, data)));
 fetch_list.push(fetch(`pack/wiki_link.json`).then(response => response.json()).then(data => Object.assign(wiki_link, data)));
 
 // local
-const cid_object = Object.create(null);
+const md_card_list = Object.create(null);
 const jp_object = Object.create(null);
 const en_object = Object.create(null);
 const pack_list = Object.create(null);
 const ltable_ocg = Object.create(null);
 const ltable_tcg = Object.create(null);
-const from_local = false;
+let from_local = false;
 if (localStorage.getItem("last_pack") === last_pack) {
 	try {
-		Object.assign(cid_object, JSON.parse(localStorage.getItem("cid_table")));
+		Object.assign(md_card_list, JSON.parse(localStorage.getItem("md_card_list")));
 		Object.assign(jp_object, JSON.parse(localStorage.getItem("name_table_jp")));
 		Object.assign(en_object, JSON.parse(localStorage.getItem("name_table_en")));
 		Object.assign(pack_list, JSON.parse(localStorage.getItem("pack_list")));
@@ -87,7 +86,7 @@ if (localStorage.getItem("last_pack") === last_pack) {
 }
 if (!from_local) {
 	localStorage.clear();
-	fetch_list.push(fetch(`text/cid_table.json`).then(response => response.json()).then(data => Object.assign(cid_object, data)));
+	fetch_list.push(fetch(`text/CardList.json`).then(response => response.json()).then(data => Object.assign(md_card_list, data)));
 	fetch_list.push(fetch(`text/name_table_jp.json`).then(response => response.json()).then(data => Object.assign(jp_object, data)));
 	fetch_list.push(fetch(`text/name_table_en.json`).then(response => response.json()).then(data => Object.assign(en_object, data)));
 	fetch_list.push(fetch(`pack/pack_list.json`).then(response => response.json()).then(data => Object.assign(pack_list, data)));
@@ -113,7 +112,7 @@ const db_ready = Promise.all(fetch_list)
 		id_to_cid = inverse_mapping(cid_table);
 		if (!from_local) {
 			try {
-				localStorage.setItem("cid_table", JSON.stringify(cid_object));
+				localStorage.setItem("md_card_list", JSON.stringify(md_card_list));
 				localStorage.setItem("name_table_jp", JSON.stringify(jp_object));
 				localStorage.setItem("name_table_en", JSON.stringify(en_object));
 				localStorage.setItem("pack_list", JSON.stringify(pack_list));
